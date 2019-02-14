Havoc-OS/android_packages_apps_HavocSettings

Add Flashlight blink on incoming calls
- Thanks to @Shripal17 for suggesting list preference
  instead of a toggle
- Add a device specific flashlight check

src/com/havoc/settings/fragments/Notifications.java
@@ -56,7 +56,10 @@ public class Notifications extends SettingsPreferenceFragment
 
     private static final String INCALL_VIB_OPTIONS = "incall_vib_options";
     private static final String DISABLE_IMMERSIVE_MESSAGE = "disable_immersive_message"; 
+    private static final String FLASHLIGHT_ON_CALL = "flashlight_on_call";
+
     private SwitchPreference mDisableIM; 
+    private ListPreference mFlashlightOnCall;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -77,6 +80,19 @@ public class Notifications extends SettingsPreferenceFragment
         int DisableIM = Settings.System.getInt(getContentResolver(), 
                 DISABLE_IMMERSIVE_MESSAGE, 0); 
         mDisableIM.setChecked(DisableIM != 0); 
+
+        
+        mFlashlightOnCall = (ListPreference) findPreference(FLASHLIGHT_ON_CALL);
+        Preference FlashOnCall = findPreference("flashlight_on_call");
+        int flashlightValue = Settings.System.getInt(getContentResolver(),
+                Settings.System.FLASHLIGHT_ON_CALL, 0);
+        mFlashlightOnCall.setValue(String.valueOf(flashlightValue));
+        mFlashlightOnCall.setSummary(mFlashlightOnCall.getEntry());
+        mFlashlightOnCall.setOnPreferenceChangeListener(this);
+
+        if (!Utils.deviceSupportsFlashLight(getActivity())) {
+            prefSet.removePreference(FlashOnCall);
+        }
     }
 
     @Override
@@ -91,7 +107,14 @@ public class Notifications extends SettingsPreferenceFragment
             Settings.System.putInt(getContentResolver(), DISABLE_IMMERSIVE_MESSAGE, 
                     value ? 1 : 0); 
             return true; 
-        }  
+        } else if (preference == mFlashlightOnCall) {
+            int flashlightValue = Integer.parseInt(((String) objValue).toString());
+            Settings.System.putInt(getContentResolver(),
+                  Settings.System.FLASHLIGHT_ON_CALL, flashlightValue);
+            mFlashlightOnCall.setValue(String.valueOf(flashlightValue));
+            mFlashlightOnCall.setSummary(mFlashlightOnCall.getEntry());
+            return true;
+        }
         return false;
     }
 

Juancarlospr5/android_packages_apps_ArsenicSettings

src/com/arsenic/settings/fragments/Notifications.java
@@ -34,11 +34,15 @@ import com.arsenic.settings.preferences.Utils;
 
 import com.arsenic.settings.R;
 
-public class Notifications extends SettingsPreferenceFragment {
+public class Notifications extends SettingsPreferenceFragment 
+        implements Preference.OnPreferenceChangeListener {
 
     public static final String TAG = "Notifications";
 	
     private static final String INCALL_VIB_OPTIONS = "incall_vib_options";
+    private static final String FLASHLIGHT_ON_CALL = "flashlight_on_call";
+
+    private ListPreference mFlashlightOnCall;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -52,6 +56,36 @@ public class Notifications extends SettingsPreferenceFragment {
         if (!Utils.isVoiceCapable(getActivity())) {
             prefScreen.removePreference(incallVibCategory);
         }
+
+        mFlashlightOnCall = (ListPreference) findPreference(FLASHLIGHT_ON_CALL);
+        Preference FlashOnCall = findPreference("flashlight_on_call");
+        int flashlightValue = Settings.System.getInt(getContentResolver(),
+                Settings.System.FLASHLIGHT_ON_CALL, 0);
+        mFlashlightOnCall.setValue(String.valueOf(flashlightValue));
+        mFlashlightOnCall.setSummary(mFlashlightOnCall.getEntry());
+        mFlashlightOnCall.setOnPreferenceChangeListener(this);
+
+        if (!Utils.deviceSupportsFlashLight(getActivity())) {
+            prefScreen.removePreference(FlashOnCall);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mFlashlightOnCall) {
+            int flashlightValue = Integer.parseInt(((String) objValue).toString());
+            Settings.System.putInt(getContentResolver(),
+                  Settings.System.FLASHLIGHT_ON_CALL, flashlightValue);
+            mFlashlightOnCall.setValue(String.valueOf(flashlightValue));
+            mFlashlightOnCall.setSummary(mFlashlightOnCall.getEntry());
+            return true;
+        }
+        return false;
     }
 
     @Override