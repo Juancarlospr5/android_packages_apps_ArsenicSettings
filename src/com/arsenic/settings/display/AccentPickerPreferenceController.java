Havoc-OS/android_packages_apps_HavocSettings

Move in the remaining theme settings

src/com/havoc/settings/display/AccentPickerPreferenceController.java
@@ -1,0 +1,94 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.havoc.settings.display;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.os.UserHandle;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceClickListener;
+import android.support.v7.preference.PreferenceScreen;
+
+import com.android.settings.core.PreferenceControllerMixin;
+
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.core.lifecycle.Lifecycle;
+import com.android.settingslib.core.lifecycle.LifecycleObserver;
+import com.android.settingslib.core.lifecycle.events.OnResume;
+
+import com.android.settings.havoc.AccentPicker;
+
+public class AccentPickerPreferenceController extends AbstractPreferenceController
+        implements PreferenceControllerMixin, LifecycleObserver, OnResume {
+
+    private static final String KEY_ACCENT_PICKER_FRAGMENT_PREF = "accent_picker";
+    private static final int MY_USER_ID = UserHandle.myUserId();
+
+    private final Fragment mParent;
+    private Preference mAccentPickerPref;
+
+    public AccentPickerPreferenceController(Context context, Lifecycle lifecycle, Fragment parent) {
+        super(context);
+        mParent = parent;
+        if (lifecycle != null) {
+            lifecycle.addObserver(this);
+        }
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        mAccentPickerPref  = (Preference) screen.findPreference(KEY_ACCENT_PICKER_FRAGMENT_PREF);
+    }
+
+    @Override
+    public void onResume() {
+        updateEnableState();
+        updateSummary();
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_ACCENT_PICKER_FRAGMENT_PREF;
+    }
+
+    public void updateEnableState() {
+        if (mAccentPickerPref == null) {
+            return;
+        }
+
+        mAccentPickerPref.setOnPreferenceClickListener(
+            new OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    AccentPicker.show(mParent);
+                    return true;
+                }
+            });
+    }
+
+    public void updateSummary() {
+        if (mAccentPickerPref != null) {
+                mAccentPickerPref.setSummary(mContext.getString(
+                        com.android.settings.R.string.theme_accent_picker_summary));
+        }
+    }
+
+}

src/com/havoc/settings/display/AutoDarkUIPreferenceController.java
@@ -1,0 +1,106 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package com.havoc.settings.display;
+
+import android.app.UiModeManager;
+import android.content.Context;
+import android.support.annotation.VisibleForTesting;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+
+import com.android.settings.R;
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settingslib.development.DeveloperOptionsPreferenceController;
+
+public class AutoDarkUIPreferenceController extends DeveloperOptionsPreferenceController
+        implements Preference.OnPreferenceChangeListener, PreferenceControllerMixin {
+
+    private static final String DARK_UI_KEY = "dark_ui_mode";
+    private final UiModeManager mUiModeManager;
+
+    public AutoDarkUIPreferenceController(Context context) {
+        this(context, context.getSystemService(UiModeManager.class));
+    }
+
+    @VisibleForTesting
+    AutoDarkUIPreferenceController(Context context, UiModeManager uiModeManager) {
+        super(context);
+        mUiModeManager = uiModeManager;
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return DARK_UI_KEY;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        mUiModeManager.setNightMode(modeToInt((String) newValue));
+        updateSummary(preference);
+        return true;
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        updateSummary(preference);
+    }
+
+    private void updateSummary(Preference preference) {
+        int mode = mUiModeManager.getNightMode();
+        ((ListPreference) preference).setValue(modeToString(mode));
+        preference.setSummary(modeToDescription(mode));
+    }
+
+    private String modeToDescription(int mode) {
+        String[] values = mContext.getResources().getStringArray(R.array.dark_ui_mode_entries);
+        switch (mode) {
+            case UiModeManager.MODE_NIGHT_AUTO:
+                return values[0];
+            case UiModeManager.MODE_NIGHT_YES:
+                return values[1];
+            case UiModeManager.MODE_NIGHT_NO:
+            default:
+                return values[2];
+
+        }
+    }
+
+    private String modeToString(int mode) {
+        switch (mode) {
+            case UiModeManager.MODE_NIGHT_AUTO:
+                return "auto";
+            case UiModeManager.MODE_NIGHT_YES:
+                return "yes";
+            case UiModeManager.MODE_NIGHT_NO:
+            default:
+                return "no";
+
+        }
+    }
+
+    private int modeToInt(String mode) {
+        switch (mode) {
+            case "auto":
+                return UiModeManager.MODE_NIGHT_AUTO;
+            case "yes":
+                return UiModeManager.MODE_NIGHT_YES;
+            case "no":
+            default:
+                return UiModeManager.MODE_NIGHT_NO;
+        }
+    }
+}

src/com/havoc/settings/display/DarkUIPreferenceController.java
@@ -1,0 +1,91 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.havoc.settings.display;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.os.ServiceManager;
+import android.os.RemoteException;
+import android.text.TextUtils;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.provider.Settings;
+
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.drawer.SettingsDrawerActivity;
+import com.android.internal.statusbar.IStatusBarService;
+
+import libcore.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.android.settings.R;
+import android.content.Intent;
+import android.os.Handler;
+import android.widget.Toast;
+
+public class DarkUIPreferenceController extends AbstractPreferenceController implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String SYSTEM_UI_THEME = "systemui_theme_style";
+    private ListPreference mSystemUiThemeStyle;
+    private IStatusBarService mStatusBarService;
+
+    public DarkUIPreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return SYSTEM_UI_THEME;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mSystemUiThemeStyle = (ListPreference) screen.findPreference(SYSTEM_UI_THEME);
+        int systemuiThemeStyle = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.SYSTEM_UI_THEME, 0);
+        int valueIndex = mSystemUiThemeStyle.findIndexOfValue(String.valueOf(systemuiThemeStyle));
+        mSystemUiThemeStyle.setValueIndex(valueIndex >= 0 ? valueIndex : 0);
+        mSystemUiThemeStyle.setSummary(mSystemUiThemeStyle.getEntry());
+        mSystemUiThemeStyle.setOnPreferenceChangeListener(this);
+    }
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mSystemUiThemeStyle) {
+            String value = (String) newValue;
+            Settings.System.putInt(mContext.getContentResolver(), Settings.System.SYSTEM_UI_THEME, Integer.valueOf(value));
+            int valueIndex = mSystemUiThemeStyle.findIndexOfValue(value);
+            mSystemUiThemeStyle.setSummary(mSystemUiThemeStyle.getEntries()[valueIndex]);
+
+            IStatusBarService statusBarService = IStatusBarService.Stub.asInterface(ServiceManager.checkService(Context.STATUS_BAR_SERVICE));
+            if (statusBarService != null) {
+                try {
+                    statusBarService.restartUI();
+                } catch (RemoteException e) {
+                    // do nothing.
+                }
+            }
+        }
+        return true;
+    }
+}

src/com/havoc/settings/fragments/Interface.java
@@ -20,11 +20,12 @@ import android.content.Context;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.dashboard.DashboardFragment;
-import com.android.settings.display.AccentPickerPreferenceController;
-import com.android.settings.display.DarkUIPreferenceController;
-import com.havoc.settings.display.QsTileStylePreferenceController;
 import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.core.lifecycle.Lifecycle;
+import com.havoc.settings.display.AccentPickerPreferenceController;
+import com.havoc.settings.display.AutoDarkUIPreferenceController;
+import com.havoc.settings.display.DarkUIPreferenceController;
+import com.havoc.settings.display.QsTileStylePreferenceController;
 
 import com.havoc.settings.R;
 
@@ -58,6 +59,7 @@ public class Interface extends DashboardFragment {
             Context context, Lifecycle lifecycle, Fragment fragment) {
         final List<AbstractPreferenceController> controllers = new ArrayList<>();
         controllers.add(new AccentPickerPreferenceController(context, lifecycle, fragment));
+	    controllers.add(new AutoDarkUIPreferenceController(context));
         controllers.add(new DarkUIPreferenceController(context));
         controllers.add(new QsTileStylePreferenceController(context));
         return controllers;

Juancarlospr5/android_packages_apps_ArsenicSettings

src/com/arsenic/settings/display/AccentPickerPreferenceController.java
@@ -1,0 +1,94 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.arsenic.settings.display;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.os.UserHandle;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceClickListener;
+import android.support.v7.preference.PreferenceScreen;
+
+import com.android.settings.core.PreferenceControllerMixin;
+
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.core.lifecycle.Lifecycle;
+import com.android.settingslib.core.lifecycle.LifecycleObserver;
+import com.android.settingslib.core.lifecycle.events.OnResume;
+
+import com.android.settings.arsenic.AccentPicker;
+
+public class AccentPickerPreferenceController extends AbstractPreferenceController
+        implements PreferenceControllerMixin, LifecycleObserver, OnResume {
+
+    private static final String KEY_ACCENT_PICKER_FRAGMENT_PREF = "accent_picker";
+    private static final int MY_USER_ID = UserHandle.myUserId();
+
+    private final Fragment mParent;
+    private Preference mAccentPickerPref;
+
+    public AccentPickerPreferenceController(Context context, Lifecycle lifecycle, Fragment parent) {
+        super(context);
+        mParent = parent;
+        if (lifecycle != null) {
+            lifecycle.addObserver(this);
+        }
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        mAccentPickerPref  = (Preference) screen.findPreference(KEY_ACCENT_PICKER_FRAGMENT_PREF);
+    }
+
+    @Override
+    public void onResume() {
+        updateEnableState();
+        updateSummary();
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_ACCENT_PICKER_FRAGMENT_PREF;
+    }
+
+    public void updateEnableState() {
+        if (mAccentPickerPref == null) {
+            return;
+        }
+
+        mAccentPickerPref.setOnPreferenceClickListener(
+            new OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    AccentPicker.show(mParent);
+                    return true;
+                }
+            });
+    }
+
+    public void updateSummary() {
+        if (mAccentPickerPref != null) {
+                mAccentPickerPref.setSummary(mContext.getString(
+                        com.android.settings.R.string.theme_accent_picker_summary));
+        }
+    }
+
+}

src/com/arsenic/settings/display/AutoDarkUIPreferenceController.java
@@ -1,0 +1,106 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package com.arsenic.settings.display;
+
+import android.app.UiModeManager;
+import android.content.Context;
+import android.support.annotation.VisibleForTesting;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+
+import com.android.settings.R;
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settingslib.development.DeveloperOptionsPreferenceController;
+
+public class AutoDarkUIPreferenceController extends DeveloperOptionsPreferenceController
+        implements Preference.OnPreferenceChangeListener, PreferenceControllerMixin {
+
+    private static final String DARK_UI_KEY = "dark_ui_mode";
+    private final UiModeManager mUiModeManager;
+
+    public AutoDarkUIPreferenceController(Context context) {
+        this(context, context.getSystemService(UiModeManager.class));
+    }
+
+    @VisibleForTesting
+    AutoDarkUIPreferenceController(Context context, UiModeManager uiModeManager) {
+        super(context);
+        mUiModeManager = uiModeManager;
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return DARK_UI_KEY;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        mUiModeManager.setNightMode(modeToInt((String) newValue));
+        updateSummary(preference);
+        return true;
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        updateSummary(preference);
+    }
+
+    private void updateSummary(Preference preference) {
+        int mode = mUiModeManager.getNightMode();
+        ((ListPreference) preference).setValue(modeToString(mode));
+        preference.setSummary(modeToDescription(mode));
+    }
+
+    private String modeToDescription(int mode) {
+        String[] values = mContext.getResources().getStringArray(R.array.dark_ui_mode_entries);
+        switch (mode) {
+            case UiModeManager.MODE_NIGHT_AUTO:
+                return values[0];
+            case UiModeManager.MODE_NIGHT_YES:
+                return values[1];
+            case UiModeManager.MODE_NIGHT_NO:
+            default:
+                return values[2];
+
+        }
+    }
+
+    private String modeToString(int mode) {
+        switch (mode) {
+            case UiModeManager.MODE_NIGHT_AUTO:
+                return "auto";
+            case UiModeManager.MODE_NIGHT_YES:
+                return "yes";
+            case UiModeManager.MODE_NIGHT_NO:
+            default:
+                return "no";
+
+        }
+    }
+
+    private int modeToInt(String mode) {
+        switch (mode) {
+            case "auto":
+                return UiModeManager.MODE_NIGHT_AUTO;
+            case "yes":
+                return UiModeManager.MODE_NIGHT_YES;
+            case "no":
+            default:
+                return UiModeManager.MODE_NIGHT_NO;
+        }
+    }
+}

src/com/arsenic/settings/display/DarkUIPreferenceController.java
@@ -1,0 +1,91 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.arsenic.settings.display;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.os.ServiceManager;
+import android.os.RemoteException;
+import android.text.TextUtils;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.provider.Settings;
+
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.drawer.SettingsDrawerActivity;
+import com.android.internal.statusbar.IStatusBarService;
+
+import libcore.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.android.settings.R;
+import android.content.Intent;
+import android.os.Handler;
+import android.widget.Toast;
+
+public class DarkUIPreferenceController extends AbstractPreferenceController implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String SYSTEM_UI_THEME = "systemui_theme_style";
+    private ListPreference mSystemUiThemeStyle;
+    private IStatusBarService mStatusBarService;
+
+    public DarkUIPreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return SYSTEM_UI_THEME;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mSystemUiThemeStyle = (ListPreference) screen.findPreference(SYSTEM_UI_THEME);
+        int systemuiThemeStyle = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.SYSTEM_UI_THEME, 0);
+        int valueIndex = mSystemUiThemeStyle.findIndexOfValue(String.valueOf(systemuiThemeStyle));
+        mSystemUiThemeStyle.setValueIndex(valueIndex >= 0 ? valueIndex : 0);
+        mSystemUiThemeStyle.setSummary(mSystemUiThemeStyle.getEntry());
+        mSystemUiThemeStyle.setOnPreferenceChangeListener(this);
+    }
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mSystemUiThemeStyle) {
+            String value = (String) newValue;
+            Settings.System.putInt(mContext.getContentResolver(), Settings.System.SYSTEM_UI_THEME, Integer.valueOf(value));
+            int valueIndex = mSystemUiThemeStyle.findIndexOfValue(value);
+            mSystemUiThemeStyle.setSummary(mSystemUiThemeStyle.getEntries()[valueIndex]);
+
+            IStatusBarService statusBarService = IStatusBarService.Stub.asInterface(ServiceManager.checkService(Context.STATUS_BAR_SERVICE));
+            if (statusBarService != null) {
+                try {
+                    statusBarService.restartUI();
+                } catch (RemoteException e) {
+                    // do nothing.
+                }
+            }
+        }
+        return true;
+    }
+}

src/com/arsenic/settings/fragments/Interface.java
@@ -20,11 +20,12 @@ import android.content.Context;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.dashboard.DashboardFragment;
-import com.android.settings.display.AccentPickerPreferenceController;
-import com.android.settings.display.DarkUIPreferenceController;
-import com.havoc.settings.display.QsTileStylePreferenceController;
 import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.core.lifecycle.Lifecycle;
+import com.arsenic.settings.display.AccentPickerPreferenceController;
+import com.arsenic.settings.display.AutoDarkUIPreferenceController;
+import com.arsenic.settings.display.DarkUIPreferenceController;
+import com.arsenic.settings.display.QsTileStylePreferenceController;
 
 import com.arsenic.settings.R;
 
@@ -58,6 +59,7 @@ public class Interface extends DashboardFragment {
             Context context, Lifecycle lifecycle, Fragment fragment) {
         final List<AbstractPreferenceController> controllers = new ArrayList<>();
         controllers.add(new AccentPickerPreferenceController(context, lifecycle, fragment));
+	    controllers.add(new AutoDarkUIPreferenceController(context));
         controllers.add(new DarkUIPreferenceController(context));
         controllers.add(new QsTileStylePreferenceController(context));
         return controllers;