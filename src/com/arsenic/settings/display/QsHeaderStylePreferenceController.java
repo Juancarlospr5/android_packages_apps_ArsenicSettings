Havoc-OS/android_packages_apps_HavocSettings

Add QS header styles [2/3]

src/com/havoc/settings/display/QsHeaderStylePreferenceController.java
@@ -1,0 +1,73 @@
+/*
+ * Copyright (C) 2018 Havoc-OS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.havoc.settings.display;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.provider.Settings;
+
+import com.android.settingslib.core.AbstractPreferenceController;
+
+import libcore.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class QsHeaderStylePreferenceController extends AbstractPreferenceController implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String QS_HEADER_STYLE = "qs_header_style";
+    private ListPreference mQsHeaderStyle;
+
+    public QsHeaderStylePreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return QS_HEADER_STYLE;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mQsHeaderStyle = (ListPreference) screen.findPreference(QS_HEADER_STYLE);
+        int qsHeaderStyle = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.QS_HEADER_STYLE, 0);
+        int valueIndex = mQsHeaderStyle.findIndexOfValue(String.valueOf(qsHeaderStyle));
+        mQsHeaderStyle.setValueIndex(valueIndex >= 0 ? valueIndex : 0);
+        mQsHeaderStyle.setSummary(mQsHeaderStyle.getEntry());
+        mQsHeaderStyle.setOnPreferenceChangeListener(this);
+    }
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mQsHeaderStyle) {
+            String value = (String) newValue;
+            Settings.System.putInt(mContext.getContentResolver(), Settings.System.QS_HEADER_STYLE, Integer.valueOf(value));
+            int valueIndex = mQsHeaderStyle.findIndexOfValue(value);
+            mQsHeaderStyle.setSummary(mQsHeaderStyle.getEntries()[valueIndex]);
+        }
+        return true;
+    }
+}

src/com/havoc/settings/fragments/Interface.java
@@ -27,6 +27,7 @@ import com.havoc.settings.display.AutoDarkUIPreferenceController;
 import com.havoc.settings.display.ContentPaddingPreferenceController;
 import com.havoc.settings.display.DarkUIPreferenceController;
 import com.havoc.settings.display.NotificationStylePreferenceController;
+import com.havoc.settings.display.QsHeaderStylePreferenceController;
 import com.havoc.settings.display.QsTileStylePreferenceController;
 import com.havoc.settings.display.RoundedCornersPreferenceController;
 
@@ -66,6 +67,7 @@ public class Interface extends DashboardFragment {
         controllers.add(new ContentPaddingPreferenceController(context));
         controllers.add(new DarkUIPreferenceController(context));
         controllers.add(new NotificationStylePreferenceController(context));
+        controllers.add(new QsHeaderStylePreferenceController(context));
         controllers.add(new QsTileStylePreferenceController(context));
         controllers.add(new RoundedCornersPreferenceController(context));
         return controllers;

Juancarlospr5/android_packages_apps_ArsenicSettings

src/com/arsenic/settings/display/QsHeaderStylePreferenceController.java
@@ -1,0 +1,73 @@
+/*
+ * Copyright (C) 2018 Havoc-OS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.arsenic.settings.display;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.provider.Settings;
+
+import com.android.settingslib.core.AbstractPreferenceController;
+
+import libcore.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class QsHeaderStylePreferenceController extends AbstractPreferenceController implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String QS_HEADER_STYLE = "qs_header_style";
+    private ListPreference mQsHeaderStyle;
+
+    public QsHeaderStylePreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return QS_HEADER_STYLE;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mQsHeaderStyle = (ListPreference) screen.findPreference(QS_HEADER_STYLE);
+        int qsHeaderStyle = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.QS_HEADER_STYLE, 0);
+        int valueIndex = mQsHeaderStyle.findIndexOfValue(String.valueOf(qsHeaderStyle));
+        mQsHeaderStyle.setValueIndex(valueIndex >= 0 ? valueIndex : 0);
+        mQsHeaderStyle.setSummary(mQsHeaderStyle.getEntry());
+        mQsHeaderStyle.setOnPreferenceChangeListener(this);
+    }
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mQsHeaderStyle) {
+            String value = (String) newValue;
+            Settings.System.putInt(mContext.getContentResolver(), Settings.System.QS_HEADER_STYLE, Integer.valueOf(value));
+            int valueIndex = mQsHeaderStyle.findIndexOfValue(value);
+            mQsHeaderStyle.setSummary(mQsHeaderStyle.getEntries()[valueIndex]);
+        }
+        return true;
+    }
+}

src/com/arsenic/settings/fragments/Interface.java
@@ -27,6 +27,7 @@ import com.arsenic.settings.display.AutoDarkUIPreferenceController;
 import com.arsenic.settings.display.ContentPaddingPreferenceController;
 import com.arsenic.settings.display.DarkUIPreferenceController;
 import com.arsenic.settings.display.NotificationStylePreferenceController;
+import com.arsenic.settings.display.QsHeaderStylePreferenceController;
 import com.arsenic.settings.display.QsTileStylePreferenceController;
 import com.arsenic.settings.display.RoundedCornersPreferenceController;
 
@@ -66,6 +67,7 @@ public class Interface extends DashboardFragment {
         controllers.add(new ContentPaddingPreferenceController(context));
         controllers.add(new DarkUIPreferenceController(context));
         controllers.add(new NotificationStylePreferenceController(context));
+        controllers.add(new QsHeaderStylePreferenceController(context));
         controllers.add(new QsTileStylePreferenceController(context));
         controllers.add(new RoundedCornersPreferenceController(context));
         return controllers;