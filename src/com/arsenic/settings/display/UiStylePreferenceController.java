Havoc-OS/android_packages_apps_HavocSettings

settings: UI Styles [1/3]

src/com/havoc/settings/display/UiStylePreferenceController.java
@@ -1,0 +1,73 @@
+/*
+ * Copyright (C) 2018 Havoc-OS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.havoc.settings.display;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.provider.Settings;
+
+import com.android.settingslib.core.AbstractPreferenceController;
+
+import libcore.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class UiStylePreferenceController extends AbstractPreferenceController implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String UI_STYLE = "ui_style";
+    private ListPreference mUiStyle;
+
+    public UiStylePreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return UI_STYLE;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mUiStyle = (ListPreference) screen.findPreference(UI_STYLE);
+        int uiStyle = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.UI_STYLE, 0);
+        int valueIndex = mUiStyle.findIndexOfValue(String.valueOf(uiStyle));
+        mUiStyle.setValueIndex(valueIndex >= 0 ? valueIndex : 0);
+        mUiStyle.setSummary(mUiStyle.getEntry());
+        mUiStyle.setOnPreferenceChangeListener(this);
+    }
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mUiStyle) {
+            String value = (String) newValue;
+            Settings.System.putInt(mContext.getContentResolver(), Settings.System.UI_STYLE, Integer.valueOf(value));
+            int valueIndex = mUiStyle.findIndexOfValue(value);
+            mUiStyle.setSummary(mUiStyle.getEntries()[valueIndex]);
+        }
+        return true;
+    }
+}

src/com/havoc/settings/fragments/Interface.java
@@ -35,6 +35,7 @@ import com.havoc.settings.display.NotificationStylePreferenceController;
 import com.havoc.settings.display.QsHeaderStylePreferenceController;
 import com.havoc.settings.display.QsTileStylePreferenceController;
 import com.havoc.settings.display.RoundedCornersPreferenceController;
+import com.havoc.settings.display.UiStylePreferenceController;
 
 import com.havoc.settings.R;
 
@@ -107,6 +108,7 @@ public class Interface extends DashboardFragment {
         controllers.add(new QsHeaderStylePreferenceController(context));
         controllers.add(new QsTileStylePreferenceController(context));
         controllers.add(new RoundedCornersPreferenceController(context));
+        controllers.add(new UiStylePreferenceController(context));
         return controllers;
     }
 }

Juancarlospr5/android_packages_apps_ArsenicSettings

src/com/arsenic/settings/display/UiStylePreferenceController.java
@@ -1,0 +1,73 @@
+/*
+ * Copyright (C) 2018 Havoc-OS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.arsenic.settings.display;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.provider.Settings;
+
+import com.android.settingslib.core.AbstractPreferenceController;
+
+import libcore.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class UiStylePreferenceController extends AbstractPreferenceController implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String UI_STYLE = "ui_style";
+    private ListPreference mUiStyle;
+
+    public UiStylePreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return UI_STYLE;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mUiStyle = (ListPreference) screen.findPreference(UI_STYLE);
+        int uiStyle = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.UI_STYLE, 0);
+        int valueIndex = mUiStyle.findIndexOfValue(String.valueOf(uiStyle));
+        mUiStyle.setValueIndex(valueIndex >= 0 ? valueIndex : 0);
+        mUiStyle.setSummary(mUiStyle.getEntry());
+        mUiStyle.setOnPreferenceChangeListener(this);
+    }
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mUiStyle) {
+            String value = (String) newValue;
+            Settings.System.putInt(mContext.getContentResolver(), Settings.System.UI_STYLE, Integer.valueOf(value));
+            int valueIndex = mUiStyle.findIndexOfValue(value);
+            mUiStyle.setSummary(mUiStyle.getEntries()[valueIndex]);
+        }
+        return true;
+    }
+}

src/com/arsenic/settings/fragments/Interface.java
@@ -35,6 +35,7 @@ import com.arsenic.settings.display.NotificationStylePreferenceController;
 import com.arsenic.settings.display.QsHeaderStylePreferenceController;
 import com.arsenic.settings.display.QsTileStylePreferenceController;
 import com.arsenic.settings.display.RoundedCornersPreferenceController;
+import com.arsenic.settings.display.UiStylePreferenceController;
 
 import com.arsenic.settings.R;
 
@@ -107,6 +108,7 @@ public class Interface extends DashboardFragment {
         controllers.add(new QsHeaderStylePreferenceController(context));
         controllers.add(new QsTileStylePreferenceController(context));
         controllers.add(new RoundedCornersPreferenceController(context));
+        controllers.add(new UiStylePreferenceController(context));
         return controllers;
     }
 }