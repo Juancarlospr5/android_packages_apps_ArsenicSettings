Havoc-OS/android_packages_apps_HavocSettings

settings: featureize ro.recents.grid and Go Layout [2/2]
thanks google for coding like a 1st grader by adding public
member variables and making this more work then needed

Bootleggers edit: Update the code for our systemui reboot util and add Go recent style as a possible feature
Bootleggers Pie edit: Switch old pie code and re-use it for this method

src/com/havoc/settings/fragments/Recents.java
@@ -47,7 +47,7 @@ public class Recents extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
     private static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
-    private static final String RECENTS_COMPONENT_TYPE = "recents_component";
+    private static final String RECENTS_LAYOUT_STYLE_PREF = "recents_layout_style";
     private static final String IMMERSIVE_RECENTS = "immersive_recents"; 
     private static final String RECENTS_DATE = "recents_full_screen_date"; 
     private static final String RECENTS_CLOCK = "recents_full_screen_clock"; 
@@ -55,7 +55,7 @@ public class Recents extends SettingsPreferenceFragment implements
     private ListPreference mImmersiveRecents; 
     private ListPreference mRecentsClearAllLocation;
     private SwitchPreference mRecentsClearAll;
-    private ListPreference mRecentsComponentType;
+    private ListPreference mRecentsLayoutStylePref;
     private SwitchPreference mClock; 
     private SwitchPreference mDate; 
 
@@ -79,13 +79,13 @@ public class Recents extends SettingsPreferenceFragment implements
         mRecentsClearAllLocation.setSummary(mRecentsClearAllLocation.getEntry());
         mRecentsClearAllLocation.setOnPreferenceChangeListener(this);
 
-        // recents component type
-        mRecentsComponentType = (ListPreference) findPreference(RECENTS_COMPONENT_TYPE);
+        // recents layout style
+        mRecentsLayoutStylePref = (ListPreference) findPreference(RECENTS_LAYOUT_STYLE_PREF);
         int type = Settings.System.getInt(resolver,
-                Settings.System.RECENTS_COMPONENT, 0);
-        mRecentsComponentType.setValue(String.valueOf(type));
-        mRecentsComponentType.setSummary(mRecentsComponentType.getEntry());
-        mRecentsComponentType.setOnPreferenceChangeListener(this);
+                Settings.System.RECENTS_LAYOUT_STYLE, 0);
+        mRecentsLayoutStylePref.setValue(String.valueOf(type));
+        mRecentsLayoutStylePref.setSummary(mRecentsLayoutStylePref.getEntry());
+        mRecentsLayoutStylePref.setOnPreferenceChangeListener(this);
 
         mImmersiveRecents = (ListPreference) findPreference(IMMERSIVE_RECENTS); 
         int mode = Settings.System.getInt(getContentResolver(), 
@@ -118,17 +118,17 @@ public class Recents extends SettingsPreferenceFragment implements
                 Settings.System.RECENTS_CLEAR_ALL_LOCATION, value, UserHandle.USER_CURRENT);
             mRecentsClearAllLocation.setSummary(mRecentsClearAllLocation.getEntries()[index]);
             return true;
-        } else if (preference == mRecentsComponentType) {
+        } else if (preference == mRecentsLayoutStylePref) {
             int type = Integer.valueOf((String) objValue);
-            int index = mRecentsComponentType.findIndexOfValue((String) objValue);
+            int index = mRecentsLayoutStylePref.findIndexOfValue((String) objValue);
             Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.RECENTS_COMPONENT, type);
-            mRecentsComponentType.setSummary(mRecentsComponentType.getEntries()[index]);
-            if (type == 1) { // Disable swipe up gesture, if oreo type selected
-               Settings.Secure.putInt(getActivity().getContentResolver(),
+                    Settings.System.RECENTS_LAYOUT_STYLE, type);
+            mRecentsLayoutStylePref.setSummary(mRecentsLayoutStylePref.getEntries()[index]);
+            if (type != 0) { // Disable swipe up gesture, if oreo type selected
+                Settings.Secure.putInt(getActivity().getContentResolver(),
                     Settings.Secure.SWIPE_UP_TO_SWITCH_APPS_ENABLED, 0);
             }
-            HavocUtils.showSystemUiRestartDialog(getContext());
+            HavocUtils.restartSystemUi(getContext());
             return true;
         }  if (preference == mImmersiveRecents) {
             int mode = Integer.valueOf((String) objValue); 

Juancarlospr5/android_packages_apps_ArsenicSettings

src/com/arsenic/settings/fragments/Recents.java
@@ -47,7 +47,7 @@ public class Recents extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
     private static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
-    private static final String RECENTS_COMPONENT_TYPE = "recents_component";
+    private static final String RECENTS_LAYOUT_STYLE_PREF = "recents_layout_style";
     private static final String IMMERSIVE_RECENTS = "immersive_recents"; 
     private static final String RECENTS_DATE = "recents_full_screen_date"; 
     private static final String RECENTS_CLOCK = "recents_full_screen_clock"; 
@@ -55,7 +55,7 @@ public class Recents extends SettingsPreferenceFragment implements
     private ListPreference mImmersiveRecents; 
     private ListPreference mRecentsClearAllLocation;
     private SwitchPreference mRecentsClearAll;
-    private ListPreference mRecentsComponentType;
+    private ListPreference mRecentsLayoutStylePref;
     private SwitchPreference mClock; 
     private SwitchPreference mDate; 
 
@@ -79,13 +79,13 @@ public class Recents extends SettingsPreferenceFragment implements
         mRecentsClearAllLocation.setSummary(mRecentsClearAllLocation.getEntry());
         mRecentsClearAllLocation.setOnPreferenceChangeListener(this);
 
-        // recents component type
-        mRecentsComponentType = (ListPreference) findPreference(RECENTS_COMPONENT_TYPE);
+        // recents layout style
+        mRecentsLayoutStylePref = (ListPreference) findPreference(RECENTS_LAYOUT_STYLE_PREF);
         int type = Settings.System.getInt(resolver,
-                Settings.System.RECENTS_COMPONENT, 0);
-        mRecentsComponentType.setValue(String.valueOf(type));
-        mRecentsComponentType.setSummary(mRecentsComponentType.getEntry());
-        mRecentsComponentType.setOnPreferenceChangeListener(this);
+                Settings.System.RECENTS_LAYOUT_STYLE, 0);
+        mRecentsLayoutStylePref.setValue(String.valueOf(type));
+        mRecentsLayoutStylePref.setSummary(mRecentsLayoutStylePref.getEntry());
+        mRecentsLayoutStylePref.setOnPreferenceChangeListener(this);
 
         mImmersiveRecents = (ListPreference) findPreference(IMMERSIVE_RECENTS); 
         int mode = Settings.System.getInt(getContentResolver(), 
@@ -118,17 +118,17 @@ public class Recents extends SettingsPreferenceFragment implements
                 Settings.System.RECENTS_CLEAR_ALL_LOCATION, value, UserHandle.USER_CURRENT);
             mRecentsClearAllLocation.setSummary(mRecentsClearAllLocation.getEntries()[index]);
             return true;
-        } else if (preference == mRecentsComponentType) {
+        } else if (preference == mRecentsLayoutStylePref) {
             int type = Integer.valueOf((String) objValue);
-            int index = mRecentsComponentType.findIndexOfValue((String) objValue);
+            int index = mRecentsLayoutStylePref.findIndexOfValue((String) objValue);
             Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.RECENTS_COMPONENT, type);
-            mRecentsComponentType.setSummary(mRecentsComponentType.getEntries()[index]);
-            if (type == 1) { // Disable swipe up gesture, if oreo type selected
-               Settings.Secure.putInt(getActivity().getContentResolver(),
+                    Settings.System.RECENTS_LAYOUT_STYLE, type);
+            mRecentsLayoutStylePref.setSummary(mRecentsLayoutStylePref.getEntries()[index]);
+            if (type != 0) { // Disable swipe up gesture, if oreo type selected
+                Settings.Secure.putInt(getActivity().getContentResolver(),
                     Settings.Secure.SWIPE_UP_TO_SWITCH_APPS_ENABLED, 0);
             }
-            ArsenicUtils.showSystemUiRestartDialog(getContext());
+            ArsenicUtils.restartSystemUi(getContext());
             return true;
         }  if (preference == mImmersiveRecents) {
             int mode = Integer.valueOf((String) objValue); 