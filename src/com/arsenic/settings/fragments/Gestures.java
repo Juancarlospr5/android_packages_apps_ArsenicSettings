Havoc-OS/android_packages_apps_HavocSettings

[2/2] Settings: op gesture tweaks
Change-Id: Ia51487c1978613ef7b4410e33795adbf0f55801f

src/com/havoc/settings/fragments/Gestures.java
@@ -29,12 +29,15 @@ import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.WindowManagerGlobal;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.havoc.settings.preferences.SystemSettingSeekBarPreference;
 import com.havoc.settings.preferences.SystemSettingSwitchPreference;
 
 import com.android.internal.logging.nano.MetricsProto;
@@ -43,8 +46,12 @@ public class Gestures extends SettingsPreferenceFragment implements
          OnPreferenceChangeListener {
 
     private static final String USE_BOTTOM_GESTURE_NAVIGATION = "use_bottom_gesture_navigation";
+    private static final String KEY_SWIPE_LENGTH = "gesture_swipe_length";
+    private static final String KEY_SWIPE_TIMEOUT = "gesture_swipe_timeout";
 
     private SystemSettingSwitchPreference mUseBottomGestureNavigation;
+    private SystemSettingSeekBarPreference mSwipeTriggerLength;
+    private SystemSettingSeekBarPreference mSwipeTriggerTimeout;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -59,6 +66,18 @@ public class Gestures extends SettingsPreferenceFragment implements
         int useBottomGestureNavigation = Settings.System.getInt(getContentResolver(),
                 USE_BOTTOM_GESTURE_NAVIGATION, 0);
         mUseBottomGestureNavigation.setChecked(useBottomGestureNavigation != 0);
+
+        mSwipeTriggerLength = (SystemSettingSeekBarPreference) findPreference(KEY_SWIPE_LENGTH);
+        int triggerLength = Settings.System.getInt(resolver, Settings.System.BOTTOM_GESTURE_SWIPE_LIMIT,
+                getSwipeLengthInPixel(getResources().getInteger(com.android.internal.R.integer.nav_gesture_swipe_min_length)));
+        mSwipeTriggerLength.setValue(triggerLength);
+        mSwipeTriggerLength.setOnPreferenceChangeListener(this);
+
+        mSwipeTriggerTimeout = (SystemSettingSeekBarPreference) findPreference(KEY_SWIPE_TIMEOUT);
+        int triggerTimeout = Settings.System.getInt(resolver, Settings.System.BOTTOM_GESTURE_TRIGGER_TIMEOUT,
+                getResources().getInteger(com.android.internal.R.integer.nav_gesture_swipe_timout));
+        mSwipeTriggerTimeout.setValue(triggerTimeout);
+        mSwipeTriggerTimeout.setOnPreferenceChangeListener(this);
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
@@ -66,10 +85,24 @@ public class Gestures extends SettingsPreferenceFragment implements
         if (preference == mUseBottomGestureNavigation) {
             boolean value = (Boolean) newValue;
             Settings.System.putInt(getContentResolver(),
-		USE_BOTTOM_GESTURE_NAVIGATION, value ? 1 : 0);
+		            USE_BOTTOM_GESTURE_NAVIGATION, value ? 1 : 0);
+            return true;
+        } else if (preference == mSwipeTriggerLength) {
+            int value = (Integer) newValue;
+            Settings.System.putInt(resolver,
+                    Settings.System.BOTTOM_GESTURE_SWIPE_LIMIT, value);
+            return true;
+        } else if (preference == mSwipeTriggerTimeout) {
+            int value = (Integer) newValue;
+            Settings.System.putInt(resolver,
+                    Settings.System.BOTTOM_GESTURE_TRIGGER_TIMEOUT, value);
             return true;
         }
         return false;
+    }
+
+    private int getSwipeLengthInPixel(int value) {
+        return Math.round(value * getResources().getDisplayMetrics().density);
     }
 
     @Override

Juancarlospr5/android_packages_apps_ArsenicSettings

src/com/arsenic/settings/fragments/Gestures.java
@@ -29,12 +29,15 @@ import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.WindowManagerGlobal;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.arsenic.settings.preferences.SystemSettingSeekBarPreference;
 import com.arsenic.settings.preferences.SystemSettingSwitchPreference;
 
 import com.android.internal.logging.nano.MetricsProto;
@@ -43,8 +46,12 @@ public class Gestures extends SettingsPreferenceFragment implements
          OnPreferenceChangeListener {
 
     private static final String USE_BOTTOM_GESTURE_NAVIGATION = "use_bottom_gesture_navigation";
+    private static final String KEY_SWIPE_LENGTH = "gesture_swipe_length";
+    private static final String KEY_SWIPE_TIMEOUT = "gesture_swipe_timeout";
 
     private SystemSettingSwitchPreference mUseBottomGestureNavigation;
+    private SystemSettingSeekBarPreference mSwipeTriggerLength;
+    private SystemSettingSeekBarPreference mSwipeTriggerTimeout;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -59,6 +66,18 @@ public class Gestures extends SettingsPreferenceFragment implements
         int useBottomGestureNavigation = Settings.System.getInt(getContentResolver(),
                 USE_BOTTOM_GESTURE_NAVIGATION, 0);
         mUseBottomGestureNavigation.setChecked(useBottomGestureNavigation != 0);
+
+        mSwipeTriggerLength = (SystemSettingSeekBarPreference) findPreference(KEY_SWIPE_LENGTH);
+        int triggerLength = Settings.System.getInt(resolver, Settings.System.BOTTOM_GESTURE_SWIPE_LIMIT,
+                getSwipeLengthInPixel(getResources().getInteger(com.android.internal.R.integer.nav_gesture_swipe_min_length)));
+        mSwipeTriggerLength.setValue(triggerLength);
+        mSwipeTriggerLength.setOnPreferenceChangeListener(this);
+
+        mSwipeTriggerTimeout = (SystemSettingSeekBarPreference) findPreference(KEY_SWIPE_TIMEOUT);
+        int triggerTimeout = Settings.System.getInt(resolver, Settings.System.BOTTOM_GESTURE_TRIGGER_TIMEOUT,
+                getResources().getInteger(com.android.internal.R.integer.nav_gesture_swipe_timout));
+        mSwipeTriggerTimeout.setValue(triggerTimeout);
+        mSwipeTriggerTimeout.setOnPreferenceChangeListener(this);
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
@@ -66,10 +85,24 @@ public class Gestures extends SettingsPreferenceFragment implements
         if (preference == mUseBottomGestureNavigation) {
             boolean value = (Boolean) newValue;
             Settings.System.putInt(getContentResolver(),
-		USE_BOTTOM_GESTURE_NAVIGATION, value ? 1 : 0);
+		            USE_BOTTOM_GESTURE_NAVIGATION, value ? 1 : 0);
+            return true;
+        } else if (preference == mSwipeTriggerLength) {
+            int value = (Integer) newValue;
+            Settings.System.putInt(resolver,
+                    Settings.System.BOTTOM_GESTURE_SWIPE_LIMIT, value);
+            return true;
+        } else if (preference == mSwipeTriggerTimeout) {
+            int value = (Integer) newValue;
+            Settings.System.putInt(resolver,
+                    Settings.System.BOTTOM_GESTURE_TRIGGER_TIMEOUT, value);
             return true;
         }
         return false;
+    }
+
+    private int getSwipeLengthInPixel(int value) {
+        return Math.round(value * getResources().getDisplayMetrics().density);
     }
 
     @Override