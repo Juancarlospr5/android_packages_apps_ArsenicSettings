Havoc-OS/android_packages_apps_HavocSettings

settings: QS themes [2/3]

src/com/havoc/settings/display/QsAlphaPreferenceController.java
@@ -1,0 +1,74 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.havoc.settings.display;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.text.TextUtils;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.provider.Settings;
+
+import com.android.settingslib.core.AbstractPreferenceController;
+
+import com.havoc.settings.preferences.SystemSettingSeekBarPreference;
+
+import libcore.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
+
+public class QsAlphaPreferenceController extends AbstractPreferenceController implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String QS_PANEL_ALPHA = "qs_panel_alpha";
+
+    private SystemSettingSeekBarPreference mQsPanelAlpha;
+
+    public QsAlphaPreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return QS_PANEL_ALPHA;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mQsPanelAlpha = (SystemSettingSeekBarPreference) screen.findPreference(QS_PANEL_ALPHA);
+        int qsPanelAlpha = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.QS_PANEL_BG_ALPHA, 255, UserHandle.USER_CURRENT);
+        mQsPanelAlpha.setValue(qsPanelAlpha);
+        mQsPanelAlpha.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mQsPanelAlpha) {
+            int bgAlpha = (Integer) newValue;
+            Settings.System.putIntForUser(mContext.getContentResolver(),
+                    Settings.System.QS_PANEL_BG_ALPHA, bgAlpha, UserHandle.USER_CURRENT);
+        }
+        return true;
+    }
+}

src/com/havoc/settings/display/QsColorPreferenceController.java
@@ -1,0 +1,84 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.havoc.settings.display;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.text.TextUtils;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.provider.Settings;
+
+import com.android.settingslib.core.AbstractPreferenceController;
+
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+
+import libcore.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
+
+public class QsColorPreferenceController extends AbstractPreferenceController implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String QS_PANEL_COLOR = "qs_panel_color";
+    static final int DEFAULT_QS_PANEL_COLOR = 0xffffffff;
+
+    private ColorPickerPreference mQsPanelColor;
+
+    public QsColorPreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return QS_PANEL_COLOR;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        int intColor;
+        String hexColor;
+
+        mQsPanelColor = (ColorPickerPreference) screen.findPreference(QS_PANEL_COLOR);
+        mQsPanelColor.setOnPreferenceChangeListener(this);
+        intColor = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.QS_PANEL_BG_COLOR, DEFAULT_QS_PANEL_COLOR, UserHandle.USER_CURRENT);
+        hexColor = String.format("#%08x", (0xffffffff & intColor));
+        mQsPanelColor.setSummary(hexColor);
+        mQsPanelColor.setNewPreviewColor(intColor);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mQsPanelColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putIntForUser(mContext.getContentResolver(),
+                    Settings.System.QS_PANEL_BG_COLOR, intHex, UserHandle.USER_CURRENT);
+        }
+        return true;
+    }
+}

src/com/havoc/settings/fragments/Interface.java
@@ -32,6 +32,8 @@ import com.havoc.settings.display.AutoDarkUIPreferenceController;
 import com.havoc.settings.display.ContentPaddingPreferenceController;
 import com.havoc.settings.display.DarkUIPreferenceController;
 import com.havoc.settings.display.NotificationStylePreferenceController;
+import com.havoc.settings.display.QsAlphaPreferenceController;
+import com.havoc.settings.display.QsColorPreferenceController;
 import com.havoc.settings.display.QsHeaderStylePreferenceController;
 import com.havoc.settings.display.QsTileStylePreferenceController;
 import com.havoc.settings.display.RoundedCornersPreferenceController;
@@ -105,6 +107,8 @@ public class Interface extends DashboardFragment {
         controllers.add(new DarkUIPreferenceController(context));
 		controllers.add(mFontPickerPreference = new FontPickerPreferenceController(context, lifecycle, fragment));
         controllers.add(new NotificationStylePreferenceController(context));
+        controllers.add(new QsAlphaPreferenceController(context));
+        controllers.add(new QsColorPreferenceController(context));
         controllers.add(new QsHeaderStylePreferenceController(context));
         controllers.add(new QsTileStylePreferenceController(context));
         controllers.add(new RoundedCornersPreferenceController(context));

Juancarlospr5/android_packages_apps_ArsenicSettings

src/com/arsenic/settings/display/QsAlphaPreferenceController.java
@@ -1,0 +1,74 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.arsenic.settings.display;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.text.TextUtils;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.provider.Settings;
+
+import com.android.settingslib.core.AbstractPreferenceController;
+
+import com.arsenic.settings.preferences.SystemSettingSeekBarPreference;
+
+import libcore.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
+
+public class QsAlphaPreferenceController extends AbstractPreferenceController implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String QS_PANEL_ALPHA = "qs_panel_alpha";
+
+    private SystemSettingSeekBarPreference mQsPanelAlpha;
+
+    public QsAlphaPreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return QS_PANEL_ALPHA;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mQsPanelAlpha = (SystemSettingSeekBarPreference) screen.findPreference(QS_PANEL_ALPHA);
+        int qsPanelAlpha = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.QS_PANEL_BG_ALPHA, 255, UserHandle.USER_CURRENT);
+        mQsPanelAlpha.setValue(qsPanelAlpha);
+        mQsPanelAlpha.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mQsPanelAlpha) {
+            int bgAlpha = (Integer) newValue;
+            Settings.System.putIntForUser(mContext.getContentResolver(),
+                    Settings.System.QS_PANEL_BG_ALPHA, bgAlpha, UserHandle.USER_CURRENT);
+        }
+        return true;
+    }
+}

src/com/arsenic/settings/display/QsColorPreferenceController.java
@@ -1,0 +1,84 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.arsenic.settings.display;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.text.TextUtils;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.provider.Settings;
+
+import com.android.settingslib.core.AbstractPreferenceController;
+
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+
+import libcore.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
+
+public class QsColorPreferenceController extends AbstractPreferenceController implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String QS_PANEL_COLOR = "qs_panel_color";
+    static final int DEFAULT_QS_PANEL_COLOR = 0xffffffff;
+
+    private ColorPickerPreference mQsPanelColor;
+
+    public QsColorPreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return QS_PANEL_COLOR;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        int intColor;
+        String hexColor;
+
+        mQsPanelColor = (ColorPickerPreference) screen.findPreference(QS_PANEL_COLOR);
+        mQsPanelColor.setOnPreferenceChangeListener(this);
+        intColor = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.QS_PANEL_BG_COLOR, DEFAULT_QS_PANEL_COLOR, UserHandle.USER_CURRENT);
+        hexColor = String.format("#%08x", (0xffffffff & intColor));
+        mQsPanelColor.setSummary(hexColor);
+        mQsPanelColor.setNewPreviewColor(intColor);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mQsPanelColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putIntForUser(mContext.getContentResolver(),
+                    Settings.System.QS_PANEL_BG_COLOR, intHex, UserHandle.USER_CURRENT);
+        }
+        return true;
+    }
+}

src/com/arsenic/settings/fragments/Interface.java
@@ -32,6 +32,8 @@ import com.arsenic.settings.display.AutoDarkUIPreferenceController;
 import com.arsenic.settings.display.ContentPaddingPreferenceController;
 import com.arsenic.settings.display.DarkUIPreferenceController;
 import com.arsenic.settings.display.NotificationStylePreferenceController;
+import com.arsenic.settings.display.QsAlphaPreferenceController;
+import com.arsenic.settings.display.QsColorPreferenceController;
 import com.arsenic.settings.display.QsHeaderStylePreferenceController;
 import com.arsenic.settings.display.QsTileStylePreferenceController;
 import com.arsenic.settings.display.RoundedCornersPreferenceController;
@@ -105,6 +107,8 @@ public class Interface extends DashboardFragment {
         controllers.add(new DarkUIPreferenceController(context));
 		controllers.add(mFontPickerPreference = new FontPickerPreferenceController(context, lifecycle, fragment));
         controllers.add(new NotificationStylePreferenceController(context));
+        controllers.add(new QsAlphaPreferenceController(context));
+        controllers.add(new QsColorPreferenceController(context));
         controllers.add(new QsHeaderStylePreferenceController(context));
         controllers.add(new QsTileStylePreferenceController(context));
         controllers.add(new RoundedCornersPreferenceController(context));