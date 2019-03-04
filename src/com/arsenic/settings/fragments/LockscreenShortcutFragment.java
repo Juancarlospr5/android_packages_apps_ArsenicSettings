Havoc-OS/android_packages_apps_HavocSettings

Settings: add user configurable lockscreen shortcuts [2/2]
Settings: fix some icons (2/3)

Settings: add back button in actionbar on Status settings

for more consistence. We add it as drawable due that we still have 3
other
places in settings where it is missing.

We will change later....after that we should have worked every
inconsistence out.

Settings: add central slim ActionListViewSettings class back...

as well shortcutpicker helper and several other stuff which is needed
for nearly every feature we have regarding navigation or shortcuts.

Imported from kk

changes:
- Renaming to be inline with fwb part
- code cleanup
- disabled code where features are not present till now

NOTE: This patch from graphics group is needed as well
      https://gerrit.slimroms.net/#/c/19987/2

Settings: materialize grabber icon

Settings: add a default slim actions array

Settings: add default slim actions for screen off gestures

add dslv controller

a lot of our settings have drag swipe features
this is the central controller for it to have
easy access...

Settings: Materialize DSLV

Settings: its not an icon anymore...its an option

we moved reset option on dslv into the menu. So the help text is a bit
missleading.

Settings: fix landscape layout on color picker

Settings: add user configurable lockscreen shortcuts (2/2)

Change-Id: Ieaaa4784b57e0ceddc366749fe48842a54d8fc11

src/com/havoc/settings/fragments/LockscreenShortcutFragment.java
@@ -1,0 +1,88 @@
+/*
+ * Copyright (C) 2013 Slimroms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.havoc.settings.fragments;
+
+import android.os.Bundle;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
+import android.provider.Settings;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.MetricsLogger;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+
+public class LockscreenShortcutFragment extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    private static final String PREF_LOCKSCREEN_SHORTCUTS_LONGPRESS =
+            "lockscreen_shortcuts_longpress";
+
+    private SwitchPreference mLockscreenShortcutsLongpress;
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.HAVOC_SETTINGS;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.lockscreen_shortcut_fragment);
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        mLockscreenShortcutsLongpress = (SwitchPreference) findPreference(
+                PREF_LOCKSCREEN_SHORTCUTS_LONGPRESS);
+        mLockscreenShortcutsLongpress.setChecked(Settings.System.getInt(getContentResolver(),
+                Settings.System.LOCKSCREEN_SHORTCUTS_LONGPRESS, 1) == 1);
+        mLockscreenShortcutsLongpress.setOnPreferenceChangeListener(this);
+
+        setHasOptionsMenu(false);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+            ViewGroup container, Bundle savedInstanceState) {
+        final View view = super.onCreateView(inflater, container, savedInstanceState);
+        final ListView list = (ListView) view.findViewById(android.R.id.list);
+        // our container already takes care of the padding
+        if (list != null) {
+            int paddingTop = list.getPaddingTop();
+            int paddingBottom = list.getPaddingBottom();
+            list.setPadding(0, paddingTop, 0, paddingBottom);
+        }
+        return view;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mLockscreenShortcutsLongpress) {
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.LOCKSCREEN_SHORTCUTS_LONGPRESS,
+                    (Boolean) newValue ? 1 : 0);
+        }
+        return true;
+    }
+}

Juancarlospr5/android_packages_apps_ArsenicSettings

src/com/arsenic/settings/fragments/LockscreenShortcutFragment.java
@@ -1,0 +1,88 @@
+/*
+ * Copyright (C) 2013 Slimroms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.arsenic.settings.fragments;
+
+import android.os.Bundle;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
+import android.provider.Settings;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.MetricsLogger;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+
+public class LockscreenShortcutFragment extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    private static final String PREF_LOCKSCREEN_SHORTCUTS_LONGPRESS =
+            "lockscreen_shortcuts_longpress";
+
+    private SwitchPreference mLockscreenShortcutsLongpress;
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.ARSENIC_SETTINGS;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.lockscreen_shortcut_fragment);
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        mLockscreenShortcutsLongpress = (SwitchPreference) findPreference(
+                PREF_LOCKSCREEN_SHORTCUTS_LONGPRESS);
+        mLockscreenShortcutsLongpress.setChecked(Settings.System.getInt(getContentResolver(),
+                Settings.System.LOCKSCREEN_SHORTCUTS_LONGPRESS, 1) == 1);
+        mLockscreenShortcutsLongpress.setOnPreferenceChangeListener(this);
+
+        setHasOptionsMenu(false);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+            ViewGroup container, Bundle savedInstanceState) {
+        final View view = super.onCreateView(inflater, container, savedInstanceState);
+        final ListView list = (ListView) view.findViewById(android.R.id.list);
+        // our container already takes care of the padding
+        if (list != null) {
+            int paddingTop = list.getPaddingTop();
+            int paddingBottom = list.getPaddingBottom();
+            list.setPadding(0, paddingTop, 0, paddingBottom);
+        }
+        return view;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mLockscreenShortcutsLongpress) {
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.LOCKSCREEN_SHORTCUTS_LONGPRESS,
+                    (Boolean) newValue ? 1 : 0);
+        }
+        return true;
+    }
+}