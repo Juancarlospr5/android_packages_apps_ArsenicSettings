Havoc-OS/android_packages_apps_HavocSettings

Settings: Adapt for vendor_support

src/com/havoc/settings/fragments/CarrierLabelSettings.java
@@ -48,9 +48,9 @@ import com.android.settings.search.Indexable;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 
-import com.havoc.settings.preferences.SystemSettingSwitchPreference;
-import com.havoc.settings.preferences.SystemSettingSeekBarPreference;
-import net.margaritov.preference.colorpicker.ColorPickerPreference;
+import com.havoc.support.preferences.SystemSettingSwitchPreference;
+import com.havoc.support.preferences.CustomSeekBarPreference;
+import com.havoc.support.colorpicker.ColorPickerPreference;
 import com.android.internal.logging.nano.MetricsProto;
 
 import java.util.ArrayList;
@@ -70,7 +70,7 @@ public class CarrierLabelSettings extends SettingsPreferenceFragment implements
     private PreferenceScreen mCustomCarrierLabel;
     private String mCustomCarrierLabelText;
     private ColorPickerPreference mCarrierColorPicker;
-    private SystemSettingSeekBarPreference mStatusBarCarrierSize;
+    private CustomSeekBarPreference mStatusBarCarrierSize;
     private ListPreference mCarrierFontStyle;
 
     @Override
@@ -95,7 +95,7 @@ public class CarrierLabelSettings extends SettingsPreferenceFragment implements
         mCarrierColorPicker.setSummary(hexColor);
         mCarrierColorPicker.setNewPreviewColor(intColor);
 
-        mStatusBarCarrierSize = (SystemSettingSeekBarPreference) findPreference(STATUS_BAR_CARRIER_FONT_SIZE);
+        mStatusBarCarrierSize = (CustomSeekBarPreference) findPreference(STATUS_BAR_CARRIER_FONT_SIZE);
         int StatusBarCarrierSize = Settings.System.getInt(resolver,
                 Settings.System.STATUS_BAR_CARRIER_FONT_SIZE, 14);
         mStatusBarCarrierSize.setValue(StatusBarCarrierSize / 1);

src/com/havoc/settings/fragments/DozeFragment.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.havoc.settings.fragments.ui;
+package com.havoc.settings.fragments;
 
 import android.app.Activity;
 import android.content.Context;

src/com/havoc/settings/fragments/Gestures.java
@@ -37,8 +37,8 @@ import android.view.WindowManagerGlobal;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import com.havoc.settings.preferences.SystemSettingSeekBarPreference;
-import com.havoc.settings.preferences.SystemSettingSwitchPreference;
+import com.havoc.support.preferences.CustomSeekBarPreference;
+import com.havoc.support.preferences.SystemSettingSwitchPreference;
 
 import com.android.internal.logging.nano.MetricsProto;
 
@@ -50,8 +50,8 @@ public class Gestures extends SettingsPreferenceFragment implements
     private static final String KEY_SWIPE_TIMEOUT = "gesture_swipe_timeout";
 
     private SystemSettingSwitchPreference mUseBottomGestureNavigation;
-    private SystemSettingSeekBarPreference mSwipeTriggerLength;
-    private SystemSettingSeekBarPreference mSwipeTriggerTimeout;
+    private CustomSeekBarPreference mSwipeTriggerLength;
+    private CustomSeekBarPreference mSwipeTriggerTimeout;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -67,13 +67,13 @@ public class Gestures extends SettingsPreferenceFragment implements
                 USE_BOTTOM_GESTURE_NAVIGATION, 0);
         mUseBottomGestureNavigation.setChecked(useBottomGestureNavigation != 0);
 
-        mSwipeTriggerLength = (SystemSettingSeekBarPreference) findPreference(KEY_SWIPE_LENGTH);
+        mSwipeTriggerLength = (CustomSeekBarPreference) findPreference(KEY_SWIPE_LENGTH);
         int triggerLength = Settings.System.getInt(resolver, Settings.System.BOTTOM_GESTURE_SWIPE_LIMIT,
                 getSwipeLengthInPixel(getResources().getInteger(com.android.internal.R.integer.nav_gesture_swipe_min_length)));
         mSwipeTriggerLength.setValue(triggerLength);
         mSwipeTriggerLength.setOnPreferenceChangeListener(this);
 
-        mSwipeTriggerTimeout = (SystemSettingSeekBarPreference) findPreference(KEY_SWIPE_TIMEOUT);
+        mSwipeTriggerTimeout = (CustomSeekBarPreference) findPreference(KEY_SWIPE_TIMEOUT);
         int triggerTimeout = Settings.System.getInt(resolver, Settings.System.BOTTOM_GESTURE_TRIGGER_TIMEOUT,
                 getResources().getInteger(com.android.internal.R.integer.nav_gesture_swipe_timout));
         mSwipeTriggerTimeout.setValue(triggerTimeout);

Juancarlospr5/android_packages_apps_ArsenicSettings

src/com/arsenic/settings/fragments/CarrierLabelSettings.java
@@ -48,9 +48,9 @@ import com.android.settings.search.Indexable;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 
-import com.arsenic.settings.preferences.SystemSettingSwitchPreference;
-import com.arsenic.settings.preferences.SystemSettingSeekBarPreference;
-import net.margaritov.preference.colorpicker.ColorPickerPreference;
+import com.arsenic.support.preferences.SystemSettingSwitchPreference;
+import com.arsenic.support.preferences.CustomSeekBarPreference;
+import com.arsenic.support.colorpicker.ColorPickerPreference;
 import com.android.internal.logging.nano.MetricsProto;
 
 import java.util.ArrayList;
@@ -70,7 +70,7 @@ public class CarrierLabelSettings extends SettingsPreferenceFragment implements
     private PreferenceScreen mCustomCarrierLabel;
     private String mCustomCarrierLabelText;
     private ColorPickerPreference mCarrierColorPicker;
-    private SystemSettingSeekBarPreference mStatusBarCarrierSize;
+    private CustomSeekBarPreference mStatusBarCarrierSize;
     private ListPreference mCarrierFontStyle;
 
     @Override
@@ -95,7 +95,7 @@ public class CarrierLabelSettings extends SettingsPreferenceFragment implements
         mCarrierColorPicker.setSummary(hexColor);
         mCarrierColorPicker.setNewPreviewColor(intColor);
 
-        mStatusBarCarrierSize = (SystemSettingSeekBarPreference) findPreference(STATUS_BAR_CARRIER_FONT_SIZE);
+        mStatusBarCarrierSize = (CustomSeekBarPreference) findPreference(STATUS_BAR_CARRIER_FONT_SIZE);
         int StatusBarCarrierSize = Settings.System.getInt(resolver,
                 Settings.System.STATUS_BAR_CARRIER_FONT_SIZE, 14);
         mStatusBarCarrierSize.setValue(StatusBarCarrierSize / 1);

src/com/arsenic/settings/fragments/DozeFragment.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.arsenic.settings.fragments.ui;
+package com.arsenic.settings.fragments;
 
 import android.app.Activity;
 import android.content.Context;

src/com/arsenic/settings/fragments/Gestures.java
@@ -37,8 +37,8 @@ import android.view.WindowManagerGlobal;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import com.arsenic.settings.preferences.SystemSettingSeekBarPreference;
-import com.arsenic.settings.preferences.SystemSettingSwitchPreference;
+import com.arsenic.support.preferences.CustomSeekBarPreference;
+import com.arsenic.support.preferences.SystemSettingSwitchPreference;
 
 import com.android.internal.logging.nano.MetricsProto;
 
@@ -50,8 +50,8 @@ public class Gestures extends SettingsPreferenceFragment implements
     private static final String KEY_SWIPE_TIMEOUT = "gesture_swipe_timeout";
 
     private SystemSettingSwitchPreference mUseBottomGestureNavigation;
-    private SystemSettingSeekBarPreference mSwipeTriggerLength;
-    private SystemSettingSeekBarPreference mSwipeTriggerTimeout;
+    private CustomSeekBarPreference mSwipeTriggerLength;
+    private CustomSeekBarPreference mSwipeTriggerTimeout;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -67,13 +67,13 @@ public class Gestures extends SettingsPreferenceFragment implements
                 USE_BOTTOM_GESTURE_NAVIGATION, 0);
         mUseBottomGestureNavigation.setChecked(useBottomGestureNavigation != 0);
 
-        mSwipeTriggerLength = (SystemSettingSeekBarPreference) findPreference(KEY_SWIPE_LENGTH);
+        mSwipeTriggerLength = (CustomSeekBarPreference) findPreference(KEY_SWIPE_LENGTH);
         int triggerLength = Settings.System.getInt(resolver, Settings.System.BOTTOM_GESTURE_SWIPE_LIMIT,
                 getSwipeLengthInPixel(getResources().getInteger(com.android.internal.R.integer.nav_gesture_swipe_min_length)));
         mSwipeTriggerLength.setValue(triggerLength);
         mSwipeTriggerLength.setOnPreferenceChangeListener(this);
 
-        mSwipeTriggerTimeout = (SystemSettingSeekBarPreference) findPreference(KEY_SWIPE_TIMEOUT);
+        mSwipeTriggerTimeout = (CustomSeekBarPreference) findPreference(KEY_SWIPE_TIMEOUT);
         int triggerTimeout = Settings.System.getInt(resolver, Settings.System.BOTTOM_GESTURE_TRIGGER_TIMEOUT,
                 getResources().getInteger(com.android.internal.R.integer.nav_gesture_swipe_timout));
         mSwipeTriggerTimeout.setValue(triggerTimeout);