Havoc-OS/android_packages_apps_HavocSettings

Settings: Add CustomDoze

src/com/havoc/settings/fragments/ui/DozeFragment.java
@@ -28,6 +28,7 @@ import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
 
 import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.util.havoc.HavocUtils;
 import com.android.settings.SettingsPreferenceFragment;
 
 import com.android.settings.R;
@@ -35,12 +36,20 @@ import com.android.settings.R;
 public class DozeFragment extends SettingsPreferenceFragment {
 
     public static final String TAG = "DozeFragment";
+    private static final String KEY_AMBIENT_DISPLAY_CUSTOM = "ambient_display_custom";
+
+    private Preference mCustomDoze;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.doze_settings);
+
+        mCustomDoze = (Preference) findPreference(KEY_AMBIENT_DISPLAY_CUSTOM);
+        if (!getResources().getBoolean(com.android.internal.R.bool.config_alt_ambient_display)) {
+            getPreferenceScreen().removePreference(mCustomDoze);
+        }
     }
 
     @Override

Juancarlospr5/android_packages_apps_ArsenicSettings

src/com/arsenic/settings/fragments/ui/DozeFragment.java
@@ -28,6 +28,7 @@ import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
 
 import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.util.arsenic.ArsenicUtils;
 import com.android.settings.SettingsPreferenceFragment;
 
 import com.android.settings.R;
@@ -35,12 +36,20 @@ import com.android.settings.R;
 public class DozeFragment extends SettingsPreferenceFragment {
 
     public static final String TAG = "DozeFragment";
+    private static final String KEY_AMBIENT_DISPLAY_CUSTOM = "ambient_display_custom";
+
+    private Preference mCustomDoze;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.doze_settings);
+
+        mCustomDoze = (Preference) findPreference(KEY_AMBIENT_DISPLAY_CUSTOM);
+        if (!getResources().getBoolean(com.android.internal.R.bool.config_alt_ambient_display)) {
+            getPreferenceScreen().removePreference(mCustomDoze);
+        }
     }
 
     @Override