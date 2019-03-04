Havoc-OS/android_packages_apps_HavocSettings

settings: Switch styles [2/3]

src/com/havoc/settings/display/SwitchStylePreferenceController.java
@@ -1,0 +1,72 @@
+/*
+ * Copyright (C) 2018 Havoc-OS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless reqswitchred by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.havoc.settings.display;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.provider.Settings;
+
+import com.android.settingslib.core.AbstractPreferenceController;
+
+import libcore.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SwitchStylePreferenceController extends AbstractPreferenceController implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String SWITCH_STYLE = "switch_style";
+    private ListPreference mSwitchStyle;
+
+    public SwitchStylePreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return SWITCH_STYLE;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mSwitchStyle = (ListPreference) screen.findPreference(SWITCH_STYLE);
+        int switchStyle = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.SWITCH_STYLE, 2);
+        int valueIndex = mSwitchStyle.findIndexOfValue(String.valueOf(switchStyle));
+        mSwitchStyle.setValueIndex(valueIndex >= 0 ? valueIndex : 0);
+        mSwitchStyle.setSummary(mSwitchStyle.getEntry());
+        mSwitchStyle.setOnPreferenceChangeListener(this);
+    }
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mSwitchStyle) {
+            String value = (String) newValue;
+            Settings.System.putInt(mContext.getContentResolver(), Settings.System.SWITCH_STYLE, Integer.valueOf(value));
+            int valueIndex = mSwitchStyle.findIndexOfValue(value);
+            mSwitchStyle.setSummary(mSwitchStyle.getEntries()[valueIndex]);
+        }
+        return true;
+    }
+}

src/com/havoc/settings/fragments/Interface.java
@@ -37,6 +37,7 @@ import com.havoc.settings.display.QsColorPreferenceController;
 import com.havoc.settings.display.QsHeaderStylePreferenceController;
 import com.havoc.settings.display.QsTileStylePreferenceController;
 import com.havoc.settings.display.RoundedCornersPreferenceController;
+import com.havoc.settings.display.SwitchStylePreferenceController;
 import com.havoc.settings.display.UiStylePreferenceController;
 
 import com.havoc.settings.R;
@@ -112,6 +113,7 @@ public class Interface extends DashboardFragment {
         controllers.add(new QsHeaderStylePreferenceController(context));
         controllers.add(new QsTileStylePreferenceController(context));
         controllers.add(new RoundedCornersPreferenceController(context));
+        controllers.add(new SwitchStylePreferenceController(context));
         controllers.add(new UiStylePreferenceController(context));
         return controllers;
     }

Juancarlospr5/android_packages_apps_ArsenicSettings

src/com/arsenic/settings/display/SwitchStylePreferenceController.java
@@ -1,0 +1,72 @@
+/*
+ * Copyright (C) 2018 Havoc-OS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless reqswitchred by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.arsenic.settings.display;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.provider.Settings;
+
+import com.android.settingslib.core.AbstractPreferenceController;
+
+import libcore.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SwitchStylePreferenceController extends AbstractPreferenceController implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String SWITCH_STYLE = "switch_style";
+    private ListPreference mSwitchStyle;
+
+    public SwitchStylePreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return SWITCH_STYLE;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mSwitchStyle = (ListPreference) screen.findPreference(SWITCH_STYLE);
+        int switchStyle = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.SWITCH_STYLE, 2);
+        int valueIndex = mSwitchStyle.findIndexOfValue(String.valueOf(switchStyle));
+        mSwitchStyle.setValueIndex(valueIndex >= 0 ? valueIndex : 0);
+        mSwitchStyle.setSummary(mSwitchStyle.getEntry());
+        mSwitchStyle.setOnPreferenceChangeListener(this);
+    }
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mSwitchStyle) {
+            String value = (String) newValue;
+            Settings.System.putInt(mContext.getContentResolver(), Settings.System.SWITCH_STYLE, Integer.valueOf(value));
+            int valueIndex = mSwitchStyle.findIndexOfValue(value);
+            mSwitchStyle.setSummary(mSwitchStyle.getEntries()[valueIndex]);
+        }
+        return true;
+    }
+}

src/com/arsenic/settings/fragments/Interface.java
@@ -37,6 +37,7 @@ import com.arsenic.settings.display.QsColorPreferenceController;
 import com.arsenic.settings.display.QsHeaderStylePreferenceController;
 import com.arsenic.settings.display.QsTileStylePreferenceController;
 import com.arsenic.settings.display.RoundedCornersPreferenceController;
+import com.arsenic.settings.display.SwitchStylePreferenceController;
 import com.arsenic.settings.display.UiStylePreferenceController;
 
 import com.arsenic.settings.R;
@@ -112,6 +113,7 @@ public class Interface extends DashboardFragment {
         controllers.add(new QsHeaderStylePreferenceController(context));
         controllers.add(new QsTileStylePreferenceController(context));
         controllers.add(new RoundedCornersPreferenceController(context));
+        controllers.add(new SwitchStylePreferenceController(context));
         controllers.add(new UiStylePreferenceController(context));
         return controllers;
     }