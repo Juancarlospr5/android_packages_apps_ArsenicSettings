Havoc-OS/android_packages_apps_HavocSettings

Settings: Custom theming [2/3]
@SKULSHADY:

- Added a theme color picker based on DU's fruity pebbles

src/com/havoc/settings/display/ThemePicker.java
@@ -1,0 +1,218 @@
+/*
+ * Copyright (C) 2018 The Dirty Unicorns Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.havoc.settings.display;
+
+import android.app.ActivityManager;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.Fragment;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.om.IOverlayManager;
+import android.os.Bundle;
+import android.os.ServiceManager;
+import android.os.RemoteException;
+import android.provider.Settings;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.statusbar.ThemeAccentUtils;
+
+import com.android.settings.R;
+import com.android.settings.core.instrumentation.InstrumentedDialogFragment;
+import com.android.settingslib.drawer.SettingsDrawerActivity;
+
+public class ThemePicker extends InstrumentedDialogFragment implements OnClickListener {
+
+    private static final String TAG_THEME_PICKER = "theme_picker";
+
+    private View mView;
+
+    private IOverlayManager mOverlayManager;
+    private int mCurrentUserId;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mOverlayManager = IOverlayManager.Stub.asInterface(
+                ServiceManager.getService(Context.OVERLAY_SERVICE));
+        mCurrentUserId = ActivityManager.getCurrentUser();
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        mView = LayoutInflater.from(getActivity()).inflate(R.layout.theme_picker, null);
+
+        if (mView != null) {
+            initView();
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setView(mView)
+                .setNegativeButton(R.string.cancel, this)
+                .setCancelable(false);
+
+        AlertDialog dialog = builder.create();
+        dialog.setCanceledOnTouchOutside(true);
+        return dialog;
+    }
+
+    private void initView() {
+
+        Button userThemeOne = mView.findViewById(R.id.userThemeOne);
+        setTheme("1", userThemeOne);
+
+        Button userThemeTwo = mView.findViewById(R.id.userThemeTwo);
+        setTheme("2", userThemeTwo);
+
+        Button userThemeThree = mView.findViewById(R.id.userThemeThree);
+        setTheme("3", userThemeThree);
+
+        Button userThemeFour = mView.findViewById(R.id.userThemeFour);
+        setTheme("4", userThemeFour);
+
+        Button userThemeFive = mView.findViewById(R.id.userThemeFive);
+        setTheme("5", userThemeFive);
+
+        Button userThemeSix = mView.findViewById(R.id.userThemeSix);
+        setTheme("6", userThemeSix);
+
+        Button userThemeSeven = mView.findViewById(R.id.userThemeSeven);
+        setTheme("7", userThemeSeven);
+
+        Button userThemeEight = mView.findViewById(R.id.userThemeEight);
+        setTheme("8", userThemeEight);
+
+        Button userThemeNine = mView.findViewById(R.id.userThemeNine);
+        setTheme("9", userThemeNine);
+
+        Button userThemeTen = mView.findViewById(R.id.userThemeTen);
+        setTheme("10", userThemeTen);
+
+        Button userThemeEleven = mView.findViewById(R.id.userThemeEleven);
+        setTheme("11", userThemeEleven);
+
+        Button userThemeTwelve = mView.findViewById(R.id.userThemeTwelve);
+        setTheme("12", userThemeTwelve);
+
+        Button userThemeThirteen = mView.findViewById(R.id.userThemeThirteen);
+        setTheme("13", userThemeThirteen);
+
+        Button userThemeFourteen = mView.findViewById(R.id.userThemeFourteen);
+        setTheme("14", userThemeFourteen);
+
+        Button userThemeFifteen = mView.findViewById(R.id.userThemeFifteen);
+        setTheme("15", userThemeFifteen);
+
+        Button userThemeSixteen = mView.findViewById(R.id.userThemeSixteen);
+        setTheme("16", userThemeSixteen);
+
+        Button userThemeSeventeen = mView.findViewById(R.id.userThemeSeventeen);
+        setTheme("17", userThemeSeventeen);
+
+        Button userThemeEighteen = mView.findViewById(R.id.userThemeEighteen);
+        setTheme("18", userThemeEighteen);
+
+        Button userThemeNineteen = mView.findViewById(R.id.userThemeNineteen);
+        setTheme("19", userThemeNineteen);
+
+        Button userThemeTwenty = mView.findViewById(R.id.userThemeTwenty);
+        setTheme("20", userThemeTwenty);
+
+        Button userThemeTwentyOne = mView.findViewById(R.id.userThemeTwentyOne);
+        setTheme("21", userThemeTwentyOne);
+
+        Button userThemeTwentyTwo = mView.findViewById(R.id.userThemeTwentyTwo);
+        setTheme("22", userThemeTwentyTwo);
+
+        Button userThemeTwentyThree = mView.findViewById(R.id.userThemeTwentyThree);
+        setTheme("23", userThemeTwentyThree);
+
+        Button userThemeTwentyFour = mView.findViewById(R.id.userThemeTwentyFour);
+        setTheme("24", userThemeTwentyFour);
+
+        Button userThemeTwentyFive = mView.findViewById(R.id.userThemeTwentyFive);
+        setTheme("25", userThemeTwentyFive);
+
+        Button userThemeTwentySix = mView.findViewById(R.id.userThemeTwentySix);
+        setTheme("26", userThemeTwentySix);
+
+        Button userThemeTwentySeven = mView.findViewById(R.id.userThemeTwentySeven);
+        setTheme("27", userThemeTwentySeven);
+
+        Button userThemeTwentyEight = mView.findViewById(R.id.userThemeTwentyEight);
+        setTheme("28", userThemeTwentyEight);
+
+        Button userThemeTwentyNine = mView.findViewById(R.id.userThemeTwentyNine);
+        setTheme("29", userThemeTwentyNine);
+
+        Button userThemeThirty = mView.findViewById(R.id.userThemeThirty);
+        setTheme("30", userThemeThirty);
+
+        Button userThemeThirtyOne = mView.findViewById(R.id.userThemeThirtyOne);
+        setTheme("31", userThemeThirtyOne);
+
+        Button userThemeThirtyTwo = mView.findViewById(R.id.userThemeThirtyTwo);
+        setTheme("32", userThemeThirtyTwo);
+
+    }
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        if (which == AlertDialog.BUTTON_NEGATIVE) {
+            dismiss();
+        }
+        if (which == AlertDialog.BUTTON_NEUTRAL) {
+            Settings.System.putIntForUser(resolver,
+                    Settings.System.THEME_PICKER, 0, mCurrentUserId);
+            dismiss();
+        }
+    }
+
+    public static void show(Fragment parent) {
+        if (!parent.isAdded()) return;
+
+        final ThemePicker dialog = new ThemePicker();
+        dialog.setTargetFragment(parent, 0);
+        dialog.show(parent.getFragmentManager(), TAG_THEME_PICKER);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.HAVOC_SETTINGS;
+    }
+
+    private void setTheme(final String theme, final Button buttonTheme) {
+        final ContentResolver resolver = getActivity().getContentResolver();
+        if (buttonTheme != null) {
+            buttonTheme.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Settings.System.putIntForUser(resolver,
+                            Settings.System.THEME_PICKER, Integer.parseInt(theme), mCurrentUserId);
+                    dismiss();
+                }
+            });
+        }
+    }
+}

src/com/havoc/settings/display/ThemePreferenceController.java
@@ -1,0 +1,94 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.havoc.settings.display;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.os.UserHandle;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceClickListener;
+import android.support.v7.preference.PreferenceScreen;
+
+import com.android.settings.core.PreferenceControllerMixin;
+
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.core.lifecycle.Lifecycle;
+import com.android.settingslib.core.lifecycle.LifecycleObserver;
+import com.android.settingslib.core.lifecycle.events.OnResume;
+
+import com.havoc.settings.display.ThemePicker;
+
+public class ThemePreferenceController extends AbstractPreferenceController implements
+        PreferenceControllerMixin, LifecycleObserver, OnResume {
+
+    private static final String THEME_PICKER = "theme_picker";
+
+    private static final int MY_USER_ID = UserHandle.myUserId();
+
+    private final Fragment mParent;
+    private Preference mThemeStyle;
+
+    public ThemePreferenceController(Context context, Lifecycle lifecycle, Fragment parent) {
+        super(context);
+        mParent = parent;
+        if (lifecycle != null) {
+            lifecycle.addObserver(this);
+        }
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        mThemeStyle = (Preference) screen.findPreference(THEME_PICKER);
+        mThemeStyle.setEnabled(true);
+    }
+
+    @Override
+    public void onResume() {
+        updateEnableState();
+        updateSummary();
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return THEME_PICKER;
+    }
+
+    public void updateEnableState() {
+        if (mThemeStyle == null) {
+            return;
+        }
+
+        mThemeStyle.setOnPreferenceClickListener(
+            new OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    ThemePicker.show(mParent);
+                    return true;
+                }
+            }
+        );
+    }
+
+    public void updateSummary() {
+        if (mThemeStyle != null) {
+            mThemeStyle.setSummary(mContext.getString(
+                    com.android.settings.R.string.theme_picker_summary));
+        }
+    }
+}

src/com/havoc/settings/fragments/Interface.java
@@ -35,6 +35,7 @@ import com.havoc.settings.display.QsColorPreferenceController;
 import com.havoc.settings.display.QsHeaderStylePreferenceController;
 import com.havoc.settings.display.QsTileStylePreferenceController;
 import com.havoc.settings.display.SwitchStylePreferenceController;
+import com.havoc.settings.display.ThemePreferenceController;
 import com.havoc.settings.display.UiStylePreferenceController;
 
 import com.havoc.settings.R;
@@ -108,6 +109,7 @@ public class Interface extends DashboardFragment {
         controllers.add(new QsHeaderStylePreferenceController(context));
         controllers.add(new QsTileStylePreferenceController(context));
         controllers.add(new SwitchStylePreferenceController(context));
+        controllers.add(new ThemePreferenceController(context, lifecycle, fragment));
         controllers.add(new UiStylePreferenceController(context));
         return controllers;
     }

Juancarlospr5/android_packages_apps_ArsenicSettings

src/com/arsenic/settings/display/ThemePicker.java
@@ -1,0 +1,218 @@
+/*
+ * Copyright (C) 2018 The Dirty Unicorns Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.arsenic.settings.display;
+
+import android.app.ActivityManager;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.Fragment;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.om.IOverlayManager;
+import android.os.Bundle;
+import android.os.ServiceManager;
+import android.os.RemoteException;
+import android.provider.Settings;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.statusbar.ThemeAccentUtils;
+
+import com.android.settings.R;
+import com.android.settings.core.instrumentation.InstrumentedDialogFragment;
+import com.android.settingslib.drawer.SettingsDrawerActivity;
+
+public class ThemePicker extends InstrumentedDialogFragment implements OnClickListener {
+
+    private static final String TAG_THEME_PICKER = "theme_picker";
+
+    private View mView;
+
+    private IOverlayManager mOverlayManager;
+    private int mCurrentUserId;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mOverlayManager = IOverlayManager.Stub.asInterface(
+                ServiceManager.getService(Context.OVERLAY_SERVICE));
+        mCurrentUserId = ActivityManager.getCurrentUser();
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        mView = LayoutInflater.from(getActivity()).inflate(R.layout.theme_picker, null);
+
+        if (mView != null) {
+            initView();
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setView(mView)
+                .setNegativeButton(R.string.cancel, this)
+                .setCancelable(false);
+
+        AlertDialog dialog = builder.create();
+        dialog.setCanceledOnTouchOutside(true);
+        return dialog;
+    }
+
+    private void initView() {
+
+        Button userThemeOne = mView.findViewById(R.id.userThemeOne);
+        setTheme("1", userThemeOne);
+
+        Button userThemeTwo = mView.findViewById(R.id.userThemeTwo);
+        setTheme("2", userThemeTwo);
+
+        Button userThemeThree = mView.findViewById(R.id.userThemeThree);
+        setTheme("3", userThemeThree);
+
+        Button userThemeFour = mView.findViewById(R.id.userThemeFour);
+        setTheme("4", userThemeFour);
+
+        Button userThemeFive = mView.findViewById(R.id.userThemeFive);
+        setTheme("5", userThemeFive);
+
+        Button userThemeSix = mView.findViewById(R.id.userThemeSix);
+        setTheme("6", userThemeSix);
+
+        Button userThemeSeven = mView.findViewById(R.id.userThemeSeven);
+        setTheme("7", userThemeSeven);
+
+        Button userThemeEight = mView.findViewById(R.id.userThemeEight);
+        setTheme("8", userThemeEight);
+
+        Button userThemeNine = mView.findViewById(R.id.userThemeNine);
+        setTheme("9", userThemeNine);
+
+        Button userThemeTen = mView.findViewById(R.id.userThemeTen);
+        setTheme("10", userThemeTen);
+
+        Button userThemeEleven = mView.findViewById(R.id.userThemeEleven);
+        setTheme("11", userThemeEleven);
+
+        Button userThemeTwelve = mView.findViewById(R.id.userThemeTwelve);
+        setTheme("12", userThemeTwelve);
+
+        Button userThemeThirteen = mView.findViewById(R.id.userThemeThirteen);
+        setTheme("13", userThemeThirteen);
+
+        Button userThemeFourteen = mView.findViewById(R.id.userThemeFourteen);
+        setTheme("14", userThemeFourteen);
+
+        Button userThemeFifteen = mView.findViewById(R.id.userThemeFifteen);
+        setTheme("15", userThemeFifteen);
+
+        Button userThemeSixteen = mView.findViewById(R.id.userThemeSixteen);
+        setTheme("16", userThemeSixteen);
+
+        Button userThemeSeventeen = mView.findViewById(R.id.userThemeSeventeen);
+        setTheme("17", userThemeSeventeen);
+
+        Button userThemeEighteen = mView.findViewById(R.id.userThemeEighteen);
+        setTheme("18", userThemeEighteen);
+
+        Button userThemeNineteen = mView.findViewById(R.id.userThemeNineteen);
+        setTheme("19", userThemeNineteen);
+
+        Button userThemeTwenty = mView.findViewById(R.id.userThemeTwenty);
+        setTheme("20", userThemeTwenty);
+
+        Button userThemeTwentyOne = mView.findViewById(R.id.userThemeTwentyOne);
+        setTheme("21", userThemeTwentyOne);
+
+        Button userThemeTwentyTwo = mView.findViewById(R.id.userThemeTwentyTwo);
+        setTheme("22", userThemeTwentyTwo);
+
+        Button userThemeTwentyThree = mView.findViewById(R.id.userThemeTwentyThree);
+        setTheme("23", userThemeTwentyThree);
+
+        Button userThemeTwentyFour = mView.findViewById(R.id.userThemeTwentyFour);
+        setTheme("24", userThemeTwentyFour);
+
+        Button userThemeTwentyFive = mView.findViewById(R.id.userThemeTwentyFive);
+        setTheme("25", userThemeTwentyFive);
+
+        Button userThemeTwentySix = mView.findViewById(R.id.userThemeTwentySix);
+        setTheme("26", userThemeTwentySix);
+
+        Button userThemeTwentySeven = mView.findViewById(R.id.userThemeTwentySeven);
+        setTheme("27", userThemeTwentySeven);
+
+        Button userThemeTwentyEight = mView.findViewById(R.id.userThemeTwentyEight);
+        setTheme("28", userThemeTwentyEight);
+
+        Button userThemeTwentyNine = mView.findViewById(R.id.userThemeTwentyNine);
+        setTheme("29", userThemeTwentyNine);
+
+        Button userThemeThirty = mView.findViewById(R.id.userThemeThirty);
+        setTheme("30", userThemeThirty);
+
+        Button userThemeThirtyOne = mView.findViewById(R.id.userThemeThirtyOne);
+        setTheme("31", userThemeThirtyOne);
+
+        Button userThemeThirtyTwo = mView.findViewById(R.id.userThemeThirtyTwo);
+        setTheme("32", userThemeThirtyTwo);
+
+    }
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        if (which == AlertDialog.BUTTON_NEGATIVE) {
+            dismiss();
+        }
+        if (which == AlertDialog.BUTTON_NEUTRAL) {
+            Settings.System.putIntForUser(resolver,
+                    Settings.System.THEME_PICKER, 0, mCurrentUserId);
+            dismiss();
+        }
+    }
+
+    public static void show(Fragment parent) {
+        if (!parent.isAdded()) return;
+
+        final ThemePicker dialog = new ThemePicker();
+        dialog.setTargetFragment(parent, 0);
+        dialog.show(parent.getFragmentManager(), TAG_THEME_PICKER);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.ARSENIC_SETTINGS;
+    }
+
+    private void setTheme(final String theme, final Button buttonTheme) {
+        final ContentResolver resolver = getActivity().getContentResolver();
+        if (buttonTheme != null) {
+            buttonTheme.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Settings.System.putIntForUser(resolver,
+                            Settings.System.THEME_PICKER, Integer.parseInt(theme), mCurrentUserId);
+                    dismiss();
+                }
+            });
+        }
+    }
+}

src/com/arsenic/settings/display/ThemePreferenceController.java
@@ -1,0 +1,94 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.arsenic.settings.display;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.os.UserHandle;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceClickListener;
+import android.support.v7.preference.PreferenceScreen;
+
+import com.android.settings.core.PreferenceControllerMixin;
+
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.core.lifecycle.Lifecycle;
+import com.android.settingslib.core.lifecycle.LifecycleObserver;
+import com.android.settingslib.core.lifecycle.events.OnResume;
+
+import com.arsenic.settings.display.ThemePicker;
+
+public class ThemePreferenceController extends AbstractPreferenceController implements
+        PreferenceControllerMixin, LifecycleObserver, OnResume {
+
+    private static final String THEME_PICKER = "theme_picker";
+
+    private static final int MY_USER_ID = UserHandle.myUserId();
+
+    private final Fragment mParent;
+    private Preference mThemeStyle;
+
+    public ThemePreferenceController(Context context, Lifecycle lifecycle, Fragment parent) {
+        super(context);
+        mParent = parent;
+        if (lifecycle != null) {
+            lifecycle.addObserver(this);
+        }
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        mThemeStyle = (Preference) screen.findPreference(THEME_PICKER);
+        mThemeStyle.setEnabled(true);
+    }
+
+    @Override
+    public void onResume() {
+        updateEnableState();
+        updateSummary();
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return THEME_PICKER;
+    }
+
+    public void updateEnableState() {
+        if (mThemeStyle == null) {
+            return;
+        }
+
+        mThemeStyle.setOnPreferenceClickListener(
+            new OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    ThemePicker.show(mParent);
+                    return true;
+                }
+            }
+        );
+    }
+
+    public void updateSummary() {
+        if (mThemeStyle != null) {
+            mThemeStyle.setSummary(mContext.getString(
+                    com.android.settings.R.string.theme_picker_summary));
+        }
+    }
+}

src/com/arsenic/settings/fragments/Interface.java
@@ -35,6 +35,7 @@ import com.arsenic.settings.display.QsColorPreferenceController;
 import com.arsenic.settings.display.QsHeaderStylePreferenceController;
 import com.arsenic.settings.display.QsTileStylePreferenceController;
 import com.arsenic.settings.display.SwitchStylePreferenceController;
+import com.arsenic.settings.display.ThemePreferenceController;
 import com.arsenic.settings.display.UiStylePreferenceController;
 
 import com.arsenic.settings.R;
@@ -108,6 +109,7 @@ public class Interface extends DashboardFragment {
         controllers.add(new QsHeaderStylePreferenceController(context));
         controllers.add(new QsTileStylePreferenceController(context));
         controllers.add(new SwitchStylePreferenceController(context));
+        controllers.add(new ThemePreferenceController(context, lifecycle, fragment));
         controllers.add(new UiStylePreferenceController(context));
         return controllers;
     }