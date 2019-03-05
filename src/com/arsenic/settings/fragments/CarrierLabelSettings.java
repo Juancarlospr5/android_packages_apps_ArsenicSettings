Havoc-OS/android_packages_apps_HavocSettings

Notch friendly carrier label [2/2]

src/com/havoc/settings/fragments/CarrierLabelSettings.java
@@ -52,9 +52,12 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 
 import com.havoc.support.preferences.SystemSettingSwitchPreference;
+import com.havoc.support.preferences.SystemSettingListPreference;
 import com.havoc.support.preferences.CustomSeekBarPreference;
 import com.havoc.support.colorpicker.ColorPickerPreference;
 import com.android.internal.logging.nano.MetricsProto;
+
+import com.android.internal.util.havoc.HavocUtils;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -67,6 +70,7 @@ public class CarrierLabelSettings extends SettingsPreferenceFragment implements
     private static final String STATUS_BAR_CARRIER_COLOR = "status_bar_carrier_color";
     private static final String STATUS_BAR_CARRIER_FONT_SIZE  = "status_bar_carrier_font_size";
     private static final String CARRIER_FONT_STYLE  = "status_bar_carrier_font_style";
+    private static final String KEY_CARRIER_LABEL = "status_bar_show_carrier";
 
     static final int DEFAULT_STATUS_CARRIER_COLOR = 0xFFFFFFFF;
 
@@ -75,6 +79,7 @@ public class CarrierLabelSettings extends SettingsPreferenceFragment implements
     private ColorPickerPreference mCarrierColorPicker;
     private CustomSeekBarPreference mStatusBarCarrierSize;
     private ListPreference mCarrierFontStyle;
+    private SystemSettingListPreference mShowCarrierLabel;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -89,6 +94,23 @@ public class CarrierLabelSettings extends SettingsPreferenceFragment implements
         // custom carrier label
         mCustomCarrierLabel = (PreferenceScreen) findPreference(CUSTOM_CARRIER_LABEL);
         updateCustomLabelTextSummary();
+
+        mShowCarrierLabel = (SystemSettingListPreference) findPreference(KEY_CARRIER_LABEL);
+        int showCarrierLabel = Settings.System.getInt(resolver,
+        Settings.System.STATUS_BAR_SHOW_CARRIER, 1);
+        CharSequence[] NonNotchEntries = { getResources().getString(R.string.show_carrier_disabled),
+                getResources().getString(R.string.show_carrier_keyguard),
+                getResources().getString(R.string.show_carrier_statusbar), getResources().getString(
+                        R.string.show_carrier_enabled) };
+        CharSequence[] NotchEntries = { getResources().getString(R.string.show_carrier_disabled),
+                getResources().getString(R.string.show_carrier_keyguard) };
+        CharSequence[] NonNotchValues = {"0", "1" , "2", "3"};
+        CharSequence[] NotchValues = {"0", "1"};
+        mShowCarrierLabel.setEntries(HavocUtils.hasNotch(getActivity()) ? NotchEntries : NonNotchEntries);
+        mShowCarrierLabel.setEntryValues(HavocUtils.hasNotch(getActivity()) ? NotchValues : NonNotchValues);
+        mShowCarrierLabel.setValue(String.valueOf(showCarrierLabel));
+        mShowCarrierLabel.setSummary(mShowCarrierLabel.getEntry());
+        mShowCarrierLabel.setOnPreferenceChangeListener(this);
 
         mCarrierColorPicker = (ColorPickerPreference) findPreference(STATUS_BAR_CARRIER_COLOR);
         mCarrierColorPicker.setOnPreferenceChangeListener(this);
@@ -125,7 +147,7 @@ public class CarrierLabelSettings extends SettingsPreferenceFragment implements
         super.onResume();
     }
 
-     public boolean onPreferenceChange(Preference preference, Object newValue) {
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
  		ContentResolver resolver = getActivity().getContentResolver();
         if (preference == mStatusBarCarrierSize) {
             int width = ((Integer)newValue).intValue();
@@ -150,8 +172,27 @@ public class CarrierLabelSettings extends SettingsPreferenceFragment implements
             Settings.System.putInt(resolver, Settings.System.
                 STATUS_BAR_CARRIER_FONT_STYLE, showCarrierFont);
             return true;
+        } else if (preference == mShowCarrierLabel) {
+            int value = Integer.parseInt((String) newValue);
+            updateCarrierLabelSummary(value);
+            return true;
         }
         return false;
+    }
+
+    private void updateCarrierLabelSummary(int value) {
+        Resources res = getResources();
+
+        if (value == 0) {
+            // Carrier Label disabled
+            mShowCarrierLabel.setSummary(res.getString(R.string.show_carrier_disabled));
+        } else if (value == 1) {
+            mShowCarrierLabel.setSummary(res.getString(R.string.show_carrier_keyguard));
+        } else if (value == 2) {
+            mShowCarrierLabel.setSummary(res.getString(R.string.show_carrier_statusbar));
+        } else if (value == 3) {
+            mShowCarrierLabel.setSummary(res.getString(R.string.show_carrier_enabled));
+        }
     }
 
     public boolean onPreferenceTreeClick(Preference preference) {

Juancarlospr5/android_packages_apps_ArsenicSettings

src/com/arsenic/settings/fragments/CarrierLabelSettings.java
@@ -52,9 +52,12 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 
 import com.arsenic.support.preferences.SystemSettingSwitchPreference;
+import com.arsenic.support.preferences.SystemSettingListPreference;
 import com.arsenic.support.preferences.CustomSeekBarPreference;
 import com.arsenic.support.colorpicker.ColorPickerPreference;
 import com.android.internal.logging.nano.MetricsProto;
+
+import com.android.internal.util.arsenic.ArsenicUtils;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -67,6 +70,7 @@ public class CarrierLabelSettings extends SettingsPreferenceFragment implements
     private static final String STATUS_BAR_CARRIER_COLOR = "status_bar_carrier_color";
     private static final String STATUS_BAR_CARRIER_FONT_SIZE  = "status_bar_carrier_font_size";
     private static final String CARRIER_FONT_STYLE  = "status_bar_carrier_font_style";
+    private static final String KEY_CARRIER_LABEL = "status_bar_show_carrier";
 
     static final int DEFAULT_STATUS_CARRIER_COLOR = 0xFFFFFFFF;
 
@@ -75,6 +79,7 @@ public class CarrierLabelSettings extends SettingsPreferenceFragment implements
     private ColorPickerPreference mCarrierColorPicker;
     private CustomSeekBarPreference mStatusBarCarrierSize;
     private ListPreference mCarrierFontStyle;
+    private SystemSettingListPreference mShowCarrierLabel;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -89,6 +94,23 @@ public class CarrierLabelSettings extends SettingsPreferenceFragment implements
         // custom carrier label
         mCustomCarrierLabel = (PreferenceScreen) findPreference(CUSTOM_CARRIER_LABEL);
         updateCustomLabelTextSummary();
+
+        mShowCarrierLabel = (SystemSettingListPreference) findPreference(KEY_CARRIER_LABEL);
+        int showCarrierLabel = Settings.System.getInt(resolver,
+        Settings.System.STATUS_BAR_SHOW_CARRIER, 1);
+        CharSequence[] NonNotchEntries = { getResources().getString(R.string.show_carrier_disabled),
+                getResources().getString(R.string.show_carrier_keyguard),
+                getResources().getString(R.string.show_carrier_statusbar), getResources().getString(
+                        R.string.show_carrier_enabled) };
+        CharSequence[] NotchEntries = { getResources().getString(R.string.show_carrier_disabled),
+                getResources().getString(R.string.show_carrier_keyguard) };
+        CharSequence[] NonNotchValues = {"0", "1" , "2", "3"};
+        CharSequence[] NotchValues = {"0", "1"};
+        mShowCarrierLabel.setEntries(HavocUtils.hasNotch(getActivity()) ? NotchEntries : NonNotchEntries);
+        mShowCarrierLabel.setEntryValues(HavocUtils.hasNotch(getActivity()) ? NotchValues : NonNotchValues);
+        mShowCarrierLabel.setValue(String.valueOf(showCarrierLabel));
+        mShowCarrierLabel.setSummary(mShowCarrierLabel.getEntry());
+        mShowCarrierLabel.setOnPreferenceChangeListener(this);
 
         mCarrierColorPicker = (ColorPickerPreference) findPreference(STATUS_BAR_CARRIER_COLOR);
         mCarrierColorPicker.setOnPreferenceChangeListener(this);
@@ -125,7 +147,7 @@ public class CarrierLabelSettings extends SettingsPreferenceFragment implements
         super.onResume();
     }
 
-     public boolean onPreferenceChange(Preference preference, Object newValue) {
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
  		ContentResolver resolver = getActivity().getContentResolver();
         if (preference == mStatusBarCarrierSize) {
             int width = ((Integer)newValue).intValue();
@@ -150,8 +172,27 @@ public class CarrierLabelSettings extends SettingsPreferenceFragment implements
             Settings.System.putInt(resolver, Settings.System.
                 STATUS_BAR_CARRIER_FONT_STYLE, showCarrierFont);
             return true;
+        } else if (preference == mShowCarrierLabel) {
+            int value = Integer.parseInt((String) newValue);
+            updateCarrierLabelSummary(value);
+            return true;
         }
         return false;
+    }
+
+    private void updateCarrierLabelSummary(int value) {
+        Resources res = getResources();
+
+        if (value == 0) {
+            // Carrier Label disabled
+            mShowCarrierLabel.setSummary(res.getString(R.string.show_carrier_disabled));
+        } else if (value == 1) {
+            mShowCarrierLabel.setSummary(res.getString(R.string.show_carrier_keyguard));
+        } else if (value == 2) {
+            mShowCarrierLabel.setSummary(res.getString(R.string.show_carrier_statusbar));
+        } else if (value == 3) {
+            mShowCarrierLabel.setSummary(res.getString(R.string.show_carrier_enabled));
+        }
     }
 
     public boolean onPreferenceTreeClick(Preference preference) {